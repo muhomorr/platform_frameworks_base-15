73fe414f54b95c524ebba207c9a467999a884f16
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-07-31 15:22:06 +0300

    infrastructure for the Storage Scopes feature

--- core/java/android/app/WallpaperManager.java
+++ core/java/android/app/WallpaperManager.java
@@ -100,6 +100,7 @@ import android.window.DesktopExperienceFlags;
 
 import com.android.internal.R;
 import com.android.internal.annotations.Keep;
+import com.android.internal.app.StorageScopesAppHooks;
 
 import libcore.io.IoUtils;
 
@@ -794,6 +795,11 @@ public class WallpaperManager {
                             && !CompatChanges.isChangeEnabled(THROW_ON_SECURITY_EXCEPTION)) {
                         Log.w(TAG, "No permission to access wallpaper, returning default"
                                 + " wallpaper to avoid crashing legacy app.");
+                        return getDefaultWallpaper(context, FLAG_SYSTEM);
+                    }
+
+                    if (StorageScopesAppHooks.isEnabled()) {
+                        Log.d("StorageScopes", "returning default wallpaper");
                         return getDefaultWallpaper(context, FLAG_SYSTEM);
                     }
 
@@ -2023,6 +2029,12 @@ public class WallpaperManager {
                             + " wallpaper file to avoid crashing legacy app.");
                     return getDefaultSystemWallpaperFile();
                 }
+
+                if (StorageScopesAppHooks.isEnabled()) {
+                    Log.d("StorageScopes", "returning default wallpaper file");
+                    return getDefaultSystemWallpaperFile();
+                }
+
                 if (mContext.getApplicationInfo().targetSdkVersion < Build.VERSION_CODES.O_MR1) {
                     Log.w(TAG, "No permission to access wallpaper, suppressing"
                             + " exception to avoid crashing legacy app.");
