1aa3ac0ff082909e7fe4a594ad656ed4ab1ead33
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-08-14 17:05:32 +0300

    zygote: define runtime flags that depend on exec spawning

--- core/java/com/android/internal/os/Zygote.java
+++ core/java/com/android/internal/os/Zygote.java
@@ -207,6 +207,14 @@ public final class Zygote {
     /** Whether this process sends a copy of its binder transactions to the PCC audit log. */
     public static final int AUDIT_OUTGOING_TRANSACTIONS = 1 << 27;
 
+    public static final int DISABLE_HARDENED_MALLOC = 1 << 29;
+    public static final int ENABLE_COMPAT_VA_39_BIT = 1 << 30;
+
+    // make sure to update isSimpleForkCommand() in core/jni/com_android_internal_os_ZygoteCommandBuffer.cpp
+    // when adding new flags that depend on exec spawning
+    public static final int RUNTIME_FLAGS_DEPENDENT_ON_EXEC_SPAWNING = DISABLE_HARDENED_MALLOC | ENABLE_COMPAT_VA_39_BIT;
+    public static final int CUSTOM_RUNTIME_FLAGS = DISABLE_HARDENED_MALLOC | ENABLE_COMPAT_VA_39_BIT;
+
     /** No external storage should be mounted. */
     public static final int MOUNT_EXTERNAL_NONE = IVold.REMOUNT_MODE_NONE;
     /** Default external storage should be mounted. */
@@ -1180,6 +1188,7 @@ public final class Zygote {
     @SuppressWarnings("unused")
     private static void callPostForkChildHooks(int runtimeFlags, boolean isSystemServer,
             boolean isZygote, String instructionSet) {
+        runtimeFlags &= ~CUSTOM_RUNTIME_FLAGS; // a warning is printed when an unknown flag is passed
         ZygoteHooks.postForkChild(runtimeFlags, isSystemServer, isZygote, instructionSet);
     }
 
