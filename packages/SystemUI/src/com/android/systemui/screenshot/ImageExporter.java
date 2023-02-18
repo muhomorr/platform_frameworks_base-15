e32ac02fd245c8e618fd9756496e871967ade8e1
r3g_5z <june@girlboss.ceo>
2023-02-18 13:12:40 -0500

    add toggle for screenshot timestamp EXIF metadata

--- packages/SystemUI/src/com/android/systemui/screenshot/ImageExporter.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ImageExporter.java
@@ -24,6 +24,7 @@ import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.ext.settings.ExtSettings;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.net.Uri;
@@ -509,6 +510,10 @@ public class ImageExporter {
                 throw new ImageExportException(EXIF_READ_EXCEPTION, e);
             }
 
+            if (!ExtSettings.SCREENSHOT_TIMESTAMP_EXIF.get(resolver.getContext())) {
+                captureTime = null;
+            }
+
             updateExifAttributes(exif, requestId, width, height, captureTime);
             try {
                 exif.saveAttributes();
@@ -568,11 +573,15 @@ public class ImageExporter {
     }
 
     static void updateExifAttributes(ExifInterface exif, UUID uniqueId, int width, int height,
-            ZonedDateTime captureTime) {
+            @Nullable ZonedDateTime captureTime) {
         exif.setAttribute(ExifInterface.TAG_IMAGE_UNIQUE_ID, uniqueId.toString());
 
         exif.setAttribute(ExifInterface.TAG_IMAGE_WIDTH, Integer.toString(width));
         exif.setAttribute(ExifInterface.TAG_IMAGE_LENGTH, Integer.toString(height));
+
+        if (captureTime == null) {
+            return;
+        }
 
         String dateTime = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss").format(captureTime);
         String subSec = DateTimeFormatter.ofPattern("SSS").format(captureTime);
