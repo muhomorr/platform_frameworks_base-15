b08256126f695c1d54f9ca9ee08fc1db4ce72e51
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-24 19:29:03 +0200

    add option to scramble PIN layout
    Applies to device PIN, SIM PIN and SIM PUK input screens.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6813,6 +6813,16 @@ public final class Settings {
         /** @hide */
         public static final String SCREENSHOT_TIMESTAMP_EXIF = "screenshot_timestamp_exif";
 
+        /** @hide */
+        public static final String SCRAMBLE_PIN_LAYOUT_PRIMARY =
+                "lockscreen_scramble_pin_layout";
+        /** @hide */
+        public static final String SCRAMBLE_PIN_LAYOUT_SECONDARY =
+                "lockscreen_scramble_pin_layout_secondary";
+
+        /** @hide */
+        public static final String SCRAMBLE_SIM_PIN_LAYOUT = "scramble_sim_pin_layout";
+
         // ExtSettings END
 
         // NOTE: If you add new settings here, be sure to add them to
