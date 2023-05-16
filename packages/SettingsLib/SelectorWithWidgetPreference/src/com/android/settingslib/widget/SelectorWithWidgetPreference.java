32f12a5a0bc75578173f47f1c791ccf06b2413aa
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-16 13:27:12 +0300

    SettingsLib: support replacing drawable in SelectorWithWidgetPreference

--- packages/SettingsLib/SelectorWithWidgetPreference/src/com/android/settingslib/widget/SelectorWithWidgetPreference.java
+++ packages/SettingsLib/SelectorWithWidgetPreference/src/com/android/settingslib/widget/SelectorWithWidgetPreference.java
@@ -188,6 +188,10 @@ public class SelectorWithWidgetPreference extends CheckBoxPreference {
 
         TextView title = (TextView) holder.findViewById(android.R.id.title);
         title.setMaxLines(mTitleMaxLines);
+
+        if (mExtraWidgetDrawableRes != 0) {
+            mExtraWidget.setImageResource(mExtraWidgetDrawableRes);
+        }
     }
 
     /**
@@ -290,6 +294,11 @@ public class SelectorWithWidgetPreference extends CheckBoxPreference {
         return mExtraWidget;
     }
 
+    private int mExtraWidgetDrawableRes;
+
+    public void setExtraWidgetDrawableResource(int resId) {
+        mExtraWidgetDrawableRes = resId;
+    }
 
     /**
      * Sets the maximum number of lines for the title.
