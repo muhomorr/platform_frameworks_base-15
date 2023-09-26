93046cb2756f9074d64ee76b441c84a812544c66
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-09-26 11:08:14 +0300

    add per-app setting for hardened_malloc and extended VA space

--- services/core/java/com/android/server/am/ProcessList.java
+++ services/core/java/com/android/server/am/ProcessList.java
@@ -123,6 +123,8 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManagerInternal;
 import android.content.pm.ProcessInfo;
 import android.content.res.Resources;
+import android.ext.settings.app.AswUseExtendedVaSpace;
+import android.ext.settings.app.AswUseHardenedMalloc;
 import android.graphics.Point;
 import android.net.LocalSocket;
 import android.net.LocalSocketAddress;
@@ -2096,6 +2098,14 @@ public final class ProcessList extends ProcessListInternal
                 GosPackageState ps = pmi.getGosPackageState(definingAppInfo.packageName, userId);
 
                 flatExtraArgs = ZygoteExtraArgs.createFlat(ctx, userId, definingAppInfo, ps, app.isolated);
+
+                if (!AswUseHardenedMalloc.I.get(ctx, userId, definingAppInfo, ps)) {
+                    runtimeFlags |= Zygote.DISABLE_HARDENED_MALLOC;
+                }
+
+                if (!AswUseExtendedVaSpace.I.get(ctx, userId, definingAppInfo, ps)) {
+                    runtimeFlags |= Zygote.ENABLE_COMPAT_VA_39_BIT;
+                }
             }
 
             // the per-user SELinux context must be set
