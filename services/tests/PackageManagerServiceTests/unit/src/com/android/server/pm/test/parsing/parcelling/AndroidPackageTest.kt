c306c65b09d12235fcf33f852f61ea628f651f0f
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-06-02 14:50:57 +0300

    extend excludedMethods list in AndroidPackageTest

--- services/tests/PackageManagerServiceTests/unit/src/com/android/server/pm/test/parsing/parcelling/AndroidPackageTest.kt
+++ services/tests/PackageManagerServiceTests/unit/src/com/android/server/pm/test/parsing/parcelling/AndroidPackageTest.kt
@@ -76,6 +76,8 @@ class AndroidPackageTest : ParcelableComponentTest(AndroidPackage::class, Packag
 
     override val excludedMethods = listOf(
         // Internal methods
+        "initPackageParsingHooks", "getPackageParsingHooks",
+        "setPackageExt", "ext",
         "toAppInfoToString",
         "toAppInfoWithoutState",
         "toAppInfoWithoutStateWithoutFlags",
