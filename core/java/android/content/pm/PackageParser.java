3590dfc7817f435ac61b0f239f9cee4a51c07224
Linus Tufvesson <lus@google.com>
2025-01-28 17:24:06 +0100

    HACK: allow apps compiled against CinnamonBun SDK
    Temporarily allow apps compiled against the CinnamonBun non-REL SDK to
    be installed on 37.0 REL devices. This is needed while prebuilt apps
    recompile against the REL SDK.
    
    Revert before release.
    
    Bug: 480974361
    Test: presubmit
    Flag: NONE platform SDK finalization
    Change-Id: Iea3337d3d0b282ddb2a72cc18294b2231246c898

--- core/java/android/content/pm/PackageParser.java
+++ core/java/android/content/pm/PackageParser.java
@@ -2622,6 +2622,13 @@ public class PackageParser {
             return Build.VERSION_CODES.CUR_DEVELOPMENT;
         }
 
+        // STOPSHIP: hack for the pre-release SDK
+        if (platformSdkCodenames.length == 0 && "CinnamonBun".equals(targetCode)) {
+            Slog.w(TAG, "Package requires development platform " + targetCode
+                    + ", returning current version " + Build.VERSION.SDK_INT);
+            return Build.VERSION.SDK_INT;
+        }
+
         // Otherwise, we're looking at an incompatible pre-release SDK.
         if (platformSdkCodenames.length > 0) {
             outError[0] = "Requires development platform " + targetCode
@@ -2691,6 +2698,13 @@ public class PackageParser {
         // definitely meet the minimum SDK requirement.
         if (matchTargetCode(platformSdkCodenames, minCode)) {
             return Build.VERSION_CODES.CUR_DEVELOPMENT;
+        }
+
+        // STOPSHIP: hack for the pre-release SDK
+        if (platformSdkCodenames.length == 0 && "CinnamonBun".equals(minCode)) {
+            Slog.w(TAG, "Package requires min development platform " + minCode
+                    + ", returning current version " + Build.VERSION.SDK_INT);
+            return Build.VERSION.SDK_INT;
         }
 
         // Otherwise, we're looking at an incompatible pre-release SDK.
