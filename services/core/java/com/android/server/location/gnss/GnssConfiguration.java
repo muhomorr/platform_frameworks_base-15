GrapheneOS-Archive/platform_frameworks_base-old

add GNSS SUPL setting

services/core/java/com/android/server/location/gnss/GnssConfiguration.java
@@ -17,6 +17,8 @@
 package com.android.server.location.gnss;
 
 import android.content.Context;
+import android.ext.settings.ExtSettings;
+import android.ext.settings.GnssConstants;
 import android.os.PersistableBundle;
 import android.os.SystemProperties;
 import android.telephony.CarrierConfigManager;
@@ -24,6 +26,7 @@ import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Slog;
 
 import com.android.internal.util.FrameworkStatsLog;
 
@@ -276,6 +279,8 @@ public class GnssConfiguration {
         loadPropertiesFromGpsDebugConfig(mProperties);
         mEsExtensionSec = getRangeCheckedConfigEsExtensionSec();
 
+        applyConfigOverrides(mContext, mProperties);
+
         logConfigurations();
 
         final HalInterfaceVersion gnssConfigurationIfaceVersion = getHalInterfaceVersion();
@@ -476,4 +481,23 @@ public class GnssConfiguration {
     private static native boolean native_set_satellite_blocklist(int[] constellations, int[] svIds);
 
     private static native boolean native_set_es_extension_sec(int emergencyExtensionSeconds);
+
+    private static void applyConfigOverrides(Context ctx, Properties props) {
+        final int suplMode = ExtSettings.GNSS_SUPL.get(ctx);
+
+        switch (suplMode) {
+            case GnssConstants.SUPL_SERVER_STANDARD:
+                Slog.d(TAG, "SUPL: using the standard server");
+                break;
+            case GnssConstants.SUPL_DISABLED:
+                Slog.d(TAG, "SUPL is disabled");
+                props.setProperty(CONFIG_SUPL_MODE, "0");
+                break;
+            case GnssConstants.SUPL_SERVER_GRAPHENEOS_PROXY:
+                Slog.d(TAG, "SUPL: using the GrapheneOS proxy");
+                props.setProperty(CONFIG_SUPL_HOST, "supl.grapheneos.org");
+                props.setProperty(CONFIG_SUPL_PORT, "7275");
+                break;
+        }
+    }
 }

muhomorr/platform_frameworks_base-15

services/core/java/com/android/server/location/gnss/GnssConfiguration.java
@@ -17,6 +17,7 @@
 package com.android.server.location.gnss;
 
 import android.content.Context;
+import android.ext.settings.GnssSettings;
 import android.location.flags.Flags;
 import android.os.PersistableBundle;
 import android.os.SystemProperties;
@@ -25,6 +26,7 @@ import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Slog;
 
 import com.android.internal.util.FrameworkStatsLog;
 
@@ -299,6 +301,8 @@ public class GnssConfiguration {
         loadPropertiesFromGpsDebugConfig(mProperties, DEBUG_PROPERTIES_SYSTEM_FILE);
         mEsExtensionSec = getRangeCheckedConfigEsExtensionSec();
 
+        applyConfigOverrides(mContext, mProperties);
+
         logConfigurations();
 
         final HalInterfaceVersion gnssConfigurationIfaceVersion = getHalInterfaceVersion();
@@ -515,4 +519,23 @@ public class GnssConfiguration {
     private static native boolean native_set_satellite_blocklist(int[] constellations, int[] svIds);
 
     private static native boolean native_set_es_extension_sec(int emergencyExtensionSeconds);
+
+    private static void applyConfigOverrides(Context ctx, Properties props) {
+        final int suplMode = GnssSettings.SUPL_SETTING.get(ctx);
+
+        switch (suplMode) {
+            case GnssSettings.SUPL_SERVER_STANDARD:
+                Slog.d(TAG, "SUPL: using the standard server");
+                break;
+            case GnssSettings.SUPL_DISABLED:
+                Slog.d(TAG, "SUPL is disabled");
+                props.setProperty(CONFIG_SUPL_MODE, "0");
+                break;
+            case GnssSettings.SUPL_SERVER_GRAPHENEOS_PROXY:
+                Slog.d(TAG, "SUPL: using the GrapheneOS proxy");
+                props.setProperty(CONFIG_SUPL_HOST, "supl.grapheneos.org");
+                props.setProperty(CONFIG_SUPL_PORT, "7275");
+                break;
+        }
+    }
 }