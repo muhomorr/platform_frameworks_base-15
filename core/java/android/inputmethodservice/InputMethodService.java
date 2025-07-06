dc2f65cfbb258c48a6dfba72de81bc32936e5fe9
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-07-06 23:39:10 +0300

    backport back button fix from 16 QPR1 Beta 2.1

--- core/java/android/inputmethodservice/InputMethodService.java
+++ core/java/android/inputmethodservice/InputMethodService.java
@@ -913,8 +913,10 @@ public class InputMethodService extends AbstractInputMethodService {
             // {@link #restartInput(InputConnection, EditorInfo)}.
             mImeDispatcher = params.imeDispatcher;
             if (mWindow != null) {
-                mWindow.getOnBackInvokedDispatcher().setImeOnBackInvokedDispatcher(
-                        params.imeDispatcher);
+                mWindow.getOnBackInvokedDispatcher().setImeOnBackInvokedDispatcher(mImeDispatcher);
+                if (mDecorViewVisible && mShowInputRequested) {
+                    registerDefaultOnBackInvokedCallback();
+                }
             }
         }
 
@@ -3526,10 +3528,6 @@ public class InputMethodService extends AbstractInputMethodService {
                 mInlineSuggestionSessionController.notifyOnStartInputView();
                 onStartInputView(mInputEditorInfo, restarting);
                 startExtractingText(true);
-                // Back callback is typically registered in {@link #showWindow()}, but it's possible
-                // for {@link #doStartInput()} to be called without {@link #showWindow()} so we also
-                // register here.
-                registerDefaultOnBackInvokedCallback();
             } else if (mCandidatesVisibility == View.VISIBLE) {
                 if (DEBUG) Log.v(TAG, "CALL: onStartCandidatesView");
                 mCandidatesViewStarted = true;
