1802133fc9a241a787625eda32c960ab5e73c32b
Darrell Shi <darrellshi@google.com>
2025-09-16 12:21:25 -0700

    Swipe up on hub should go to lockscreen
    This change makes it so that if the device is locked swiping up on the
    glanceable hub goes back to lockscreen instead of the bouncer.
    
    Test: atest CommunalUserActionsViewModelTest
    Test: manually verified swiping up goes to lockscreen
    Fix: 441791362
    Flag: com.android.systemui.scene_container
    Change-Id: If2ec50863bec9645f8ef8ac8a76429af2ffd1070

--- packages/SystemUI/multivalentTests/src/com/android/systemui/communal/ui/viewmodel/CommunalUserActionsViewModelTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/communal/ui/viewmodel/CommunalUserActionsViewModelTest.kt
@@ -74,8 +74,7 @@ class CommunalUserActionsViewModelTest : SysuiTestCase() {
             setUpState(isShadeTouchable = true, isDeviceUnlocked = false)
             assertThat(actions).isNotEmpty()
             assertThat(actions?.get(Swipe.End)).isEqualTo(UserActionResult(Scenes.Lockscreen))
-            assertThat(actions?.get(Swipe.Up))
-                .isEqualTo(UserActionResult.ShowOverlay(Overlays.Bouncer))
+            assertThat(actions?.get(Swipe.Up)).isEqualTo(UserActionResult(Scenes.Lockscreen))
             assertThat(actions?.get(Swipe.Down)).isEqualTo(UserActionResult(Scenes.Shade))
 
             setUpState(isShadeTouchable = false, isDeviceUnlocked = false)
@@ -97,8 +96,7 @@ class CommunalUserActionsViewModelTest : SysuiTestCase() {
             setUpState(isShadeTouchable = true, isDeviceUnlocked = false)
             assertThat(actions).isNotEmpty()
             assertThat(actions?.get(Swipe.End)).isEqualTo(UserActionResult(Scenes.Lockscreen))
-            assertThat(actions?.get(Swipe.Up))
-                .isEqualTo(UserActionResult.ShowOverlay(Overlays.Bouncer))
+            assertThat(actions?.get(Swipe.Up)).isEqualTo(UserActionResult(Scenes.Lockscreen))
             assertThat(actions?.get(Swipe.Down))
                 .isEqualTo(UserActionResult(Scenes.Shade, ToSplitShade))
 
@@ -122,8 +120,7 @@ class CommunalUserActionsViewModelTest : SysuiTestCase() {
             setUpState(isShadeTouchable = true, isDeviceUnlocked = false)
             assertThat(actions).isNotEmpty()
             assertThat(actions?.get(Swipe.End)).isEqualTo(UserActionResult(Scenes.Lockscreen))
-            assertThat(actions?.get(Swipe.Up))
-                .isEqualTo(UserActionResult.ShowOverlay(Overlays.Bouncer))
+            assertThat(actions?.get(Swipe.Up)).isEqualTo(UserActionResult(Scenes.Lockscreen))
             assertThat(actions?.get(Swipe.Down))
                 .isEqualTo(UserActionResult.ShowOverlay(Overlays.NotificationsShade))
 
