0270e7e424f7836eefe6c6f66e2b06f828884471
Jorge Gil <jorgegil@google.com>
2025-09-18 17:27:02 +0000

    Check parent is attached before restoring from requestFullscreenMode
    The mMultiWindowRestoreParent might've been removed while the
    requestFullscreenMode task was in fullscreen, so attempting to restore
    to it will raise an error in system_server.
    
    This is possible in multi-desks when a task inside the desk root task
    requests fullscreen and thus gets moved out of the desk, then if the
    desk is removed (e.g. from Overview) there restore parent of the
    fullscreen task is no longer valid and we shouldn't attempt to reparent
    it there.
    
    Flag: EXEMPT BUGFIX
    Bug: 444605677
    Test: open two apps in desktop, have one app requestFullscreenMode with
    the API, swipe up to overview and dismiss the desk the task was in, go
    back to the fullscreen app and make it exit with the API - verify
    there's no exception thrown in system server and the system is still
    usable.
    
    Change-Id: I34fd85822b9b5969a10791ec36a92ce7a938e416

--- services/core/java/com/android/server/wm/Task.java
+++ services/core/java/com/android/server/wm/Task.java
@@ -4709,7 +4709,9 @@ class Task extends TaskFragment {
                 .equals(mMultiWindowRestoreParent)) {
             // Restore previous parent if parent has changed.
             final Task parent = fromWindowContainerToken(mMultiWindowRestoreParent);
-            reparent(parent, MAX_VALUE);
+            if (parent != null && parent.isAttached()) {
+                reparent(parent, MAX_VALUE);
+            }
         }
 
         // mMultiWindowRestoreWindowingMode is INVALID for non-root tasks
