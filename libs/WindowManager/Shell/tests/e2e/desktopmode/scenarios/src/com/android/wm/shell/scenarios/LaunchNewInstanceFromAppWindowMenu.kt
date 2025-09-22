039e2af27fd52c5beaf0bc0212dd4f11dbd74beb
Souradeep <souradeepghosh@google.com>
2025-09-22 10:28:07 +0000

    b/418233959 Add a new test case for LaunchNewInstanceFromAppWindowMenuFlickerTest to flicker test
    Test: USE_RBE=false atest
    WMShellFlickerTestsDesktopMode:com.android.wm.shell.flicker.multiinstance.LaunchNewInstanceFromAppWindowMenuFlickerTest
    -c (http://ab/I88500010448713171)
    
    Video: http://recall/-/eO7nAofgKRC5MRoUitAtbD
    Flag: EXEMPT TEST_ONLY
    Bug: 418233959
    Change-Id: Iaad98f59b292647fb9c50340540c3ec90cec71e6

---
+++ libs/WindowManager/Shell/tests/e2e/desktopmode/scenarios/src/com/android/wm/shell/scenarios/LaunchNewInstanceFromAppWindowMenu.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.wm.shell.scenarios
+
+import android.app.Instrumentation
+import android.tools.Rotation
+import android.tools.device.apphelpers.BrowserAppHelper
+import android.tools.traces.parsers.WindowManagerStateHelper
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import com.android.launcher3.tapl.LauncherInstrumentation
+import com.android.server.wm.flicker.helpers.DesktopModeAppHelper
+import org.junit.After
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+
+/** Base scenario test to launch a new instance from app window menu and select the new window. */
+@Ignore("Test Base Class")
+abstract class LaunchNewInstanceFromAppWindowMenu(val rotation: Rotation = Rotation.ROTATION_0) :
+    TestScenarioBase(rotation) {
+
+    private val instrumentation: Instrumentation = InstrumentationRegistry.getInstrumentation()
+    private val tapl = LauncherInstrumentation()
+    private val wmHelper = WindowManagerStateHelper(instrumentation)
+    private val device = UiDevice.getInstance(instrumentation)
+    private val browserApp = BrowserAppHelper(instrumentation)
+    val testApp = DesktopModeAppHelper(browserApp)
+
+    @Before
+    fun setup() {
+        testApp.enterDesktopMode(wmHelper, device)
+    }
+
+    @Test
+    open fun launchNewInstance() {
+        openNewWindowFromAppWindowMenu()
+        wmHelper
+            .StateSyncBuilder()
+            .withFreeformApp(browserApp.componentMatcher)
+            .withAppTransitionIdle()
+            .waitForAndVerify()
+    }
+
+    @After
+    fun teardown() {
+        testApp.exit(wmHelper)
+    }
+
+    private fun openNewWindowFromAppWindowMenu() {
+        browserApp.openThreeDotsMenu()
+        browserApp.clickNewWindowInMenu()
+    }
+}
