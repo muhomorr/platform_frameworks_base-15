925b2f106b8444d0c9abb6bee95ecaf44d438094
Linus Tufvesson <lus@google.com>
2025-09-29 04:19:54 -0700

    Revert^2 "Add CinnamonBun as a known codename"
    This reverts commit 7b0b287ff1186954a2d10f12c11765557936944d.
    
    Reason for revert: This change is unrelated to the robolectric failure in b/447116546. Landing it as a standalone to avoid churn
    
    Bug: 446867997
    Bug: 435123598
    Change-Id: Ie9381e1bdb6fad296105ff4d52ad2665c0c96898

--- tools/aapt2/SdkConstants.cpp
+++ tools/aapt2/SdkConstants.cpp
@@ -39,6 +39,7 @@ static constexpr StringPiece sDevelopmentSdkCodeNames[] = {
     "UpsideDownCake"sv,
     "VanillaIceCream"sv,
     "Baklava"sv,
+    "CinnamonBun"sv,
     "CANARY"sv,
 };
 // clang-format on
