12527e81e03d0e0160169e748868a051d4d03d8e
Greg Kaiser <gkaiser@google.com>
2025-09-30 14:54:18 -0600

    BouncyBall: Note unit for frame rate vars/constant
    We have our code be more self-descriptive about what units we're
    using for the frame rate.
    
    With the exception of one clarifying switch to using Math.round()
    (which itself has no change when the frame rate lacks a fractional
    component), this change is purely cosmetic.
    
    Bug: 444496029
    Test: "atest --include-preview-tests GtsPerformanceHostTestCases"
    Flag: EXEMPT TEST_ONLY
    Change-Id: I2cf282311a1401493a3ded1cfd3e1f6755726904

--- tests/BouncyBall/app/src/main/java/com/android/test/bouncyball/BouncyBallActivity.java
+++ tests/BouncyBall/app/src/main/java/com/android/test/bouncyball/BouncyBallActivity.java
@@ -59,7 +59,7 @@ public class BouncyBallActivity extends Activity {
     // this a valid test.  We'll generally use the maximum of
     // getSuggestedFrameRate() and the rate the system launches us at.  But
     // if that maximum is less than this minimum, we'll use this minimum.
-    private static final float MINIMUM_TEST_FRAME_RATE = 60.0f;
+    private static final float MINIMUM_TEST_FRAME_RATE_HZ = 60.0f;
 
     // This test focuses on sustained frame rate, so we want to ignore drops
     // right at app start.  We don't want to wait too long, though, lest we
@@ -101,7 +101,7 @@ public class BouncyBallActivity extends Activity {
     private int mDisplayId = -1;
     private boolean mHasFocus = false;
     private boolean mWarmedUp = false;
-    private float mFrameRate;
+    private float mFrameRateHz;
     private int mFrameCount = 0;
     private int mMinFirstRelevantFrame = -1;
     private int mMaxFirstRelevantFrame = -1;
@@ -124,15 +124,15 @@ public class BouncyBallActivity extends Activity {
                     if (displayId != mDisplayId) {
                         return;
                     }
-                    float frameRate = getDisplay().getMode().getRefreshRate();
-                    if (frameRate == mFrameRate) {
+                    float frameRateHz = getDisplay().getMode().getRefreshRate();
+                    if (frameRateHz == mFrameRateHz) {
                         // On devices with API level < 36, we might get this
                         // called for other reasons (like brightness changing).
                         // We ignore anything but frame rate changes.
                         return;
                     }
-                    setFrameRate(frameRate);
-                    Log.i(LOG_TAG, "Using frame rate " + mFrameRate + "Hz");
+                    setFrameRate(frameRateHz);
+                    Log.i(LOG_TAG, "Using frame rate " + mFrameRateHz + "Hz");
                 }
             };
 
@@ -202,7 +202,7 @@ public class BouncyBallActivity extends Activity {
                 private void dropFrameSometimes() {
                     if ((mFrameCount % 64) == 0) {
                         // We'll sleep for 1.5 frames worth of time to force a drop.
-                        float overFrameInMillis = 1.5f * 1_000.0f / mFrameRate;
+                        float overFrameInMillis = 1.5f * 1_000.0f / mFrameRateHz;
                         try {
                             Thread.sleep((long) overFrameInMillis);
                         } catch (InterruptedException ex) {
@@ -259,37 +259,37 @@ public class BouncyBallActivity extends Activity {
         Display display = getDisplay();
         Display.Mode currentMode = display.getMode();
         mDisplayId = display.getDisplayId();
-        float minimumFrameRate = MINIMUM_TEST_FRAME_RATE;
+        float minimumFrameRateHz = MINIMUM_TEST_FRAME_RATE_HZ;
         if (Build.VERSION.SDK_INT >= 36) {
             // This API wasn't introduced until API level 36, so for testing
             // on older devices, we'll just stick with our MINIMUM.
             // TODO(b/442635053): Allow switching between NORMAL and HIGH here,
             //     so we can also test against the HIGH rate.
-            minimumFrameRate =
+            minimumFrameRateHz =
                 display.getSuggestedFrameRate(Display.FRAME_RATE_CATEGORY_NORMAL);
         }
-        if (minimumFrameRate < MINIMUM_TEST_FRAME_RATE) {
-            Log.w(LOG_TAG, "getSuggestedFrameRate (" + minimumFrameRate
-                    + "Hz) is below our testing minimum (" + MINIMUM_TEST_FRAME_RATE
+        if (minimumFrameRateHz < MINIMUM_TEST_FRAME_RATE_HZ) {
+            Log.w(LOG_TAG, "getSuggestedFrameRate (" + minimumFrameRateHz
+                    + "Hz) is below our testing minimum (" + MINIMUM_TEST_FRAME_RATE_HZ
                     + "Hz); using the latter for our minimum.");
-            minimumFrameRate = MINIMUM_TEST_FRAME_RATE;
+            minimumFrameRateHz = MINIMUM_TEST_FRAME_RATE_HZ;
         }
         setFrameRate(currentMode.getRefreshRate());
-        if (mFrameRate >= minimumFrameRate) {
+        if (mFrameRateHz >= minimumFrameRateHz) {
             // The default frame rate is sufficient for our testing.
             return;
         }
 
-        String minRateStr = minimumFrameRate + "Hz";
+        String minRateStr = minimumFrameRateHz + "Hz";
         // Using a Warning here, because this seems unexpected that a device
         // defaults to running at below this rate.
-        Log.w(LOG_TAG, "App launched with frame rate (" + mFrameRate
+        Log.w(LOG_TAG, "App launched with frame rate (" + mFrameRateHz
                   + "Hz), below the acceptable/expected minimum (" + minRateStr + ")");
 
         // If available at our current resolution, use 60Hz.  If not, use the
         // lowest refresh rate above 60Hz which is available.  Otherwise, throw
         // an exception which kills the app.
-        float preferredRate = Float.POSITIVE_INFINITY;
+        float preferredRateHz = Float.POSITIVE_INFINITY;
 
         for (Display.Mode mode : display.getSupportedModes()) {
             if ((currentMode.getPhysicalHeight() != mode.getPhysicalHeight())
@@ -297,45 +297,45 @@ public class BouncyBallActivity extends Activity {
                 // This is a different resolution; we'll skip it.
                 continue;
             }
-            float rate = mode.getRefreshRate();
-            if (rate == minimumFrameRate) {
+            float rateHz = mode.getRefreshRate();
+            if (rateHz == minimumFrameRateHz) {
                 // This is exactly what we were hoping for, so we can stop
                 // looking.
-                preferredRate = rate;
+                preferredRateHz = rateHz;
                 break;
             }
-            if ((rate > minimumFrameRate) && (rate < preferredRate)) {
+            if ((rateHz > minimumFrameRateHz) && (rateHz < preferredRateHz)) {
                 // This is the best rate we've seen so far in terms of being
                 // closest to our desired rate without being under it.
-                preferredRate = rate;
+                preferredRateHz = rateHz;
             }
         }
-        if (preferredRate == Float.POSITIVE_INFINITY) {
+        if (preferredRateHz == Float.POSITIVE_INFINITY) {
             String msg = "No display mode with at least " + minRateStr;
             throw new RuntimeException(msg);
         }
-        Log.i(LOG_TAG, "Requesting to run at " + preferredRate + "Hz");
+        Log.i(LOG_TAG, "Requesting to run at " + preferredRateHz + "Hz");
         Window window = getWindow();
         WindowManager.LayoutParams params = window.getAttributes();
-        params.preferredRefreshRate = preferredRate;
+        params.preferredRefreshRate = preferredRateHz;
         window.setAttributes(params);
     }
 
-    private void setFrameRate(float frameRate) {
-        mFrameRate = frameRate;
+    private void setFrameRate(float frameRateHz) {
+        mFrameRateHz = frameRateHz;
 
         if (mTraceState != TRACE_STATE_PRE_TEST_TIME) {
-            String msg = "Got new frame rate (" + frameRate + ") after "
+            String msg = "Got new frame rate (" + frameRateHz + ") after "
                     + mFrameCount + " frames, later than first relevant frame "
                     + mActualFirstRelevantFrame;
             reportAssumptionFailure(msg);
         }
-        Log.i(LOG_TAG, "Running at frame rate " + mFrameRate + "Hz");
+        Log.i(LOG_TAG, "Running at frame rate " + mFrameRateHz + "Hz");
 
         mMinFirstRelevantFrame =
-            Math.round(INITIAL_MIN_TIME_TO_IGNORE_IN_SECONDS * mFrameRate);
+            Math.round(INITIAL_MIN_TIME_TO_IGNORE_IN_SECONDS * mFrameRateHz);
         mMaxFirstRelevantFrame =
-            Math.round(INITIAL_MAX_TIME_TO_IGNORE_IN_SECONDS * mFrameRate);
+            Math.round(INITIAL_MAX_TIME_TO_IGNORE_IN_SECONDS * mFrameRateHz);
     }
 
     private float nanosToMillis(long nanos) {
@@ -362,7 +362,7 @@ public class BouncyBallActivity extends Activity {
             // Let's set our last frame and start testing.
             mActualFirstRelevantFrame = mFrameCount;
             mLastRelevantFrame = mActualFirstRelevantFrame
-                    + (AUTOMATED_TEST_DURATION_IN_SECONDS * (int) mFrameRate);
+                    + Math.round(AUTOMATED_TEST_DURATION_IN_SECONDS * mFrameRateHz);
             return true;
         }
 
