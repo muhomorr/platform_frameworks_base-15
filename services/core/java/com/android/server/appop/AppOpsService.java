1bc6b146137f76589146dff5cd82363de7ccfb7d
Nate Myren <ntmyren@google.com>
2025-10-07 14:03:49 -0700

    Ensure sandboxed UIDs are treated as untrusted in Appops
    They should not be considered "system" app for the purposes of
    attribution tag vaildation
    
    Bug: 443742082
    Test: atest AppOpsMemoryUsageTest
    Flag: EXEMPT CVE_FIX
    Change-Id: I0c4ac8eaa8966027ad01375dde58b05febec3ffb

--- services/core/java/com/android/server/appop/AppOpsService.java
+++ services/core/java/com/android/server/appop/AppOpsService.java
@@ -5048,6 +5048,9 @@ public class AppOpsService extends IAppOpsService.Stub {
         if (packageName == null) {
             return true;
         }
+        if (Process.isSdkSandboxUid(uid)) {
+            return false;
+        }
         int appId = UserHandle.getAppId(uid);
         if (appId > 0 && appId < Process.FIRST_APPLICATION_UID) {
             return true;
