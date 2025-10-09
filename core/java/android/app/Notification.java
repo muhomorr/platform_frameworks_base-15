4469dd84106754241be29d547d6c7e04eecc9478
Ibrahim Yilmaz <iyz@google.com>
2025-10-09 04:21:39 -0700

    MetricStyle: Get back to normal HUN templates
    We decided to move forward using the standard HUN treatments for
    MetricStyle instead of using expanded template.
    
    Bug: 35809759
    Test: manual (post a heads up MetricStyle notification with actions and observe that collapse view is shown with actions)
    Flag: android.app.api_metric_style
    Change-Id: I39cc65dfabddeffb3fd605fd531116b3b56b7b36

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -6389,7 +6389,8 @@ public class Notification implements Parcelable
                     || resId == getCollapsedMediaLayoutResource()
                     || resId == getCollapsedConversationLayoutResource()
                     || (apiMetricStyle() && (resId == getCollapsedMetricLayoutResource()
-                    || resId == getCompactHeadsUpMetricLayoutResource()))
+                    || resId == getCompactHeadsUpMetricLayoutResource()
+                    || resId == getHeadsUpMetricLayoutResource()))
                     || (notificationsRedesignTemplates()
                     && resId == getCollapsedCallLayoutResource()));
             RemoteViews contentView = new BuilderRemoteViews(mContext.getApplicationInfo(), resId);
@@ -8266,8 +8267,13 @@ public class Notification implements Parcelable
         private int getCompactHeadsUpMetricLayoutResource() {
             return R.layout.notification_2025_template_compact_heads_up_metric;
         }
+
         private int getCollapsedMetricLayoutResource() {
             return R.layout.notification_2025_template_collapsed_metric;
+        }
+
+        private int getHeadsUpMetricLayoutResource() {
+            return R.layout.notification_2025_template_heads_up_metric;
         }
 
         private int getExpandedMetricLayoutResource() {
@@ -12060,18 +12066,22 @@ public class Notification implements Parcelable
         /** @hide */
         @Override
         public RemoteViews makeHeadsUpContentView() {
-            return makeLargeMetricContentView(StandardTemplateParams.VIEW_TYPE_HEADS_UP);
+            final StandardTemplateParams p = mBuilder.mParams.reset()
+                    .viewType(StandardTemplateParams.VIEW_TYPE_HEADS_UP)
+                    .fillTextsFrom(mBuilder).text(null)
+                    .hideProgress(true)
+                    .hideRightIcon(true);
+            final TemplateBindResult result = new TemplateBindResult();
+            final RemoteViews contentView = getStandardView(
+                    mBuilder.getHeadsUpMetricLayoutResource(), p, result);
+            return bindMetricStyleMetrics(contentView, p, mMetrics, /* isExpandedView = */false);
         }
 
         /** @hide */
         @Override
         public RemoteViews makeExpandedContentView() {
-            return makeLargeMetricContentView(StandardTemplateParams.VIEW_TYPE_EXPANDED);
-        }
-
-        private RemoteViews makeLargeMetricContentView(int viewType){
             final StandardTemplateParams p = mBuilder.mParams.reset()
-                    .viewType(viewType)
+                    .viewType(StandardTemplateParams.VIEW_TYPE_EXPANDED)
                     .hideProgress(true)
                     .fillTextsFrom(mBuilder)
                     .text(null)
