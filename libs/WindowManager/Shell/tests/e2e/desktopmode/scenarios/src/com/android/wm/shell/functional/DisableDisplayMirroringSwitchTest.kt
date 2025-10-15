750ca6146d8a01c9c5b003d3097e3cf8d06f4657
Gustav Sennton <gsennton@google.com>
2025-10-15 16:10:10 +0000

    [Desktop] Add screenrecord rule usage to MirroringSwitch e2e tests
    Bug: 451458959
    Flag: EXEMPT TEST_ONLY
    Test: atest FunctionalTestsDesktopMode:com.android.wm.shell.functional.DisableDisplayMirroringSwitchTest
    Change-Id: Iadddb50678b365cd40e94d8fd097e9cd25a7dfd9

--- libs/WindowManager/Shell/tests/e2e/desktopmode/scenarios/src/com/android/wm/shell/functional/DisableDisplayMirroringSwitchTest.kt
+++ libs/WindowManager/Shell/tests/e2e/desktopmode/scenarios/src/com/android/wm/shell/functional/DisableDisplayMirroringSwitchTest.kt
@@ -18,6 +18,7 @@ package com.android.wm.shell.functional
 
 import android.platform.test.annotations.Presubmit
 import android.platform.test.annotations.RequiresDesktopDevice
+import android.platform.test.rule.ScreenRecordRule
 import com.android.wm.shell.scenarios.DisableDisplayMirroringSwitch
 import org.junit.runner.RunWith
 import org.junit.runners.BlockJUnit4ClassRunner
@@ -26,4 +27,5 @@ import org.junit.runners.BlockJUnit4ClassRunner
 @RunWith(BlockJUnit4ClassRunner::class)
 @RequiresDesktopDevice
 @Presubmit
+@ScreenRecordRule.ScreenRecord
 class DisableDisplayMirroringSwitchTest : DisableDisplayMirroringSwitch()
