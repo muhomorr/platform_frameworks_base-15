86f088ca5e7cd56fe70ea3df9d7e55a2297f7ae2
Piotr Wilczyński <wilczynskip@google.com>
2025-10-16 07:31:48 -0700

    Enable flag for testDockedDeviceState_displayStateUpdated
    Bug: 452514228
    Test: DisplayManagerServiceTest
    Flag: EXEMPT BUGFIX
    Change-Id: I8a4829b8ffeed6db88e85d2e9429cb318568bc7c

--- services/tests/displayservicetests/src/com/android/server/display/DisplayManagerServiceTest.java
+++ services/tests/displayservicetests/src/com/android/server/display/DisplayManagerServiceTest.java
@@ -1017,7 +1017,10 @@ public class DisplayManagerServiceTest {
     }
 
     @Test
-    @EnableFlags(Flags.FLAG_FIX_SET_DISPLAY_STATE_AFTER_DEVICE_CHANGE)
+    @EnableFlags({
+        Flags.FLAG_CHANGE_DEFAULT_DISPLAY_LID_CLOSED,
+        Flags.FLAG_FIX_SET_DISPLAY_STATE_AFTER_DEVICE_CHANGE
+    })
     public void testDockedDeviceState_displayStateUpdated() {
         mDisplayManager = new DisplayManagerService(mContext,
                 mShortMockedInjector);
