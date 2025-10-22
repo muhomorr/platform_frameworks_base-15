3021e05a40c2e1dce2726f99f8ac9bc23f090dc8
Fabián Kozynski <kozynski@google.com>
2025-10-22 16:07:56 -0400

    Move brightness.shared to pods
    Test: build pod and SystemUI
    Flag: EXEMPT PURE_REFACTOR
    Bug: 452957820
    Change-Id: I9a2a21ae6c707b4babdc4ec114d9008274073e38

--- packages/SystemUI/pods/src/com/android/systemui/brightness/shared/BrightnessLog.kt
+++ packages/SystemUI/pods/src/com/android/systemui/brightness/shared/BrightnessLog.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 The Android Open Source Project
+ * Copyright (C) 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 
-package com.android.systemui.brightness.shared.model
+package com.android.systemui.brightness.shared
 
 import javax.inject.Qualifier
 
 @Qualifier
 @MustBeDocumented
 @Retention(AnnotationRetention.RUNTIME)
-annotation class BrightnessLog()
+public annotation class BrightnessLog
--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -49,6 +49,7 @@ import com.android.systemui.bouncer.data.repository.BouncerRepositoryModule;
 import com.android.systemui.bouncer.domain.interactor.BouncerInteractorModule;
 import com.android.systemui.bouncer.ui.BouncerViewModule;
 import com.android.systemui.brightness.dagger.ScreenBrightnessModule;
+import com.android.systemui.brightness.impl.BrightnessModule;
 import com.android.systemui.classifier.FalsingModule;
 import com.android.systemui.clipboardoverlay.dagger.ClipboardOverlayModule;
 import com.android.systemui.common.data.CommonDataLayerModule;
@@ -231,6 +232,7 @@ import javax.inject.Named;
         BouncerInteractorModule.class,
         BouncerRepositoryModule.class,
         BouncerViewModule.class,
+        BrightnessModule.class,
         CameraProtectionModule.class,
         ClipboardOverlayModule.class,
         ClockRegistryModule.class,
