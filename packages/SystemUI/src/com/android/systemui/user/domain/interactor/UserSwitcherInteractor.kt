6e847f5fd1dfc4aef56940168e4d069955c851e9
Yao Li <yaohuali@google.com>
2025-10-24 22:45:11 +0000

    UserSwitcherInteractor: Check whether user is full in sys-ui code
    UserInfo.supportsSwitchToByUser() is equivalent to UserInfo.supportsSwitchTo() && UserInfo.isFull().
    
    Bug: 438760762
    Flag: EXEMPT PURE_REFACTOR
    Test: Manual verification: HSU doesn't appear in user-selection in login and settings
    Change-Id: I4295d4dc368f0185882f40fe4c2fffb519b4a0d0

--- packages/SystemUI/src/com/android/systemui/user/domain/interactor/UserSwitcherInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/user/domain/interactor/UserSwitcherInteractor.kt
@@ -716,7 +716,7 @@ constructor(
             // We avoid showing disabled users.
             !userInfo.isEnabled -> null
             // We meet the conditions to return the UserModel.
-            userInfo.isGuest || userInfo.supportsSwitchToByUser() ->
+            userInfo.isGuest || (userInfo.supportsSwitchTo() && userInfo.isFull()) ->
                 toUserModel(userInfo, selectedUserId, canSwitchUsers)
             else -> null
         }
