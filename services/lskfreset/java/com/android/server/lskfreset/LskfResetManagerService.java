76bc5fcb0affa33f65a2fb47c804c9aca07f8436
John Admanski <jadmanski@google.com>
2025-10-27 17:12:45 +0000

    Properly synchronize access to the recovery session API.
    In theory this shouldn't matter than much, as there shouldn't actually
    be sessions used in a concurrent way. Neverthless we should handle it
    explicitly rather than just assuming you won't use things in this way.
    
    There's a few different ways this could be done, this appraoch is a
    fairly straightforward one that effectively just serializes the calls on
    a single session.
    
    Bug: 454613215
    Test: mmma, atest LskfResetTests
    Flag: android.app.lskfreset.flags.enable_lskf_reset_manager
    Change-Id: I0c210e578a6817ed522bde20793928b9f1715848

--- services/lskfreset/java/com/android/server/lskfreset/LskfResetManagerService.java
+++ services/lskfreset/java/com/android/server/lskfreset/LskfResetManagerService.java
@@ -29,6 +29,7 @@ import android.os.RemoteException;
 import android.os.UserHandle;
 import android.util.Slog;
 
+import com.android.internal.annotations.GuardedBy;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.server.SystemService;
 
@@ -97,12 +98,12 @@ public class LskfResetManagerService extends SystemService {
                             Slog.w(
                                     STUB_TAG,
                                     "Client for session died, cleaning up for user: " + user);
-                            session.terminate();
+                            session.closeSession(null);
                         },
                         0);
             } catch (RemoteException e) {
                 Slog.e(STUB_TAG, "Failed to link to death for session, cleaning up", e);
-                session.close();
+                session.closeSession(null);
                 return null;
             }
 
@@ -116,11 +117,26 @@ public class LskfResetManagerService extends SystemService {
 
         private final UserHandle mUser;
         private final String mSessionId;
+
+        private final OpenSession mOpenSession;
+
+        @GuardedBy("mOpenSession")
         private boolean mClosed;
+
+        // Implements the actual underlying session operations with the assumption that the session
+        // has not been closed and will not be close while this is active.
+        private class OpenSession {
+            public void saveEscrowToken(@NonNull EscrowToken escrowToken) {
+                // TODO: Validate token
+                // TODO: mLskfResetKeyManager.storeEscrowToken(mUser, mSessionId, escrowToken);
+                throw new UnsupportedOperationException("Not implemented");
+            }
+        }
 
         LskfResetSessionImpl(UserHandle user) {
             mUser = user;
             mSessionId = UUID.randomUUID().toString();
+            mOpenSession = new OpenSession();
             mClosed = false;
         }
 
@@ -128,32 +144,45 @@ public class LskfResetManagerService extends SystemService {
             return mSessionId;
         }
 
-        void terminate() {
-            if (!mClosed) close();
+        // Close the underlying session if it is not closed already. Accepts an optional runnable
+        // that will be executed if the caller is trying to close an already-closed session. This is
+        // used to treat a double-close as a failure in certain contexts.
+        void closeSession(Runnable runOnAlreadyClosed) {
+            synchronized (mOpenSession) {
+                if (mClosed) {
+                    if (runOnAlreadyClosed != null) runOnAlreadyClosed.run();
+                    return;
+                }
+                // Remove from the active sessions map in the parent
+                if (mActiveSessions.remove(asBinder()) != null) {
+                    Slog.d(
+                            SESSION_TAG,
+                            "Session removed. Active sessions: " + mActiveSessions.size());
+                }
+                mClosed = true;
+            }
         }
 
         @Override
         @RequiresNoPermission
         public void saveEscrowToken(@NonNull EscrowToken escrowToken) {
             Slog.d(SESSION_TAG, "saveEscrowToken for session " + mSessionId + ", user " + mUser);
-            if (mClosed) throw new IllegalStateException("Session is already closed");
             // TODO: Permission checks
-            // TODO: Validate token
-            // TODO: mLskfResetKeyManager.storeEscrowToken(mUser, mSessionId, escrowToken);
-            throw new UnsupportedOperationException("Not implemented");
+            synchronized (mOpenSession) {
+                if (mClosed) throw new IllegalStateException("Session is already closed");
+                mOpenSession.saveEscrowToken(escrowToken);
+            }
         }
 
         @Override
         @RequiresNoPermission
         public void close() {
             Slog.d(SESSION_TAG, "close called for session " + mSessionId + ", user " + mUser);
-            if (mClosed) throw new IllegalStateException("Session is already closed");
-            // TODO: Implement lock and close mechanism.
-            // Remove from the active sessions map in the parent
-            if (mActiveSessions.remove(asBinder()) != null) {
-                Slog.d(SESSION_TAG, "Session removed. Active sessions: " + mActiveSessions.size());
-            }
-            mClosed = true;
+            // TODO: Permission checks
+            closeSession(
+                    () -> {
+                        throw new IllegalStateException("Session is already closed");
+                    });
         }
     }
 }
