08db1b203c43e669552ee17dc96b6511084e2603
Mark Punzalan <markpun@google.com>
2025-10-28 15:32:50 -0700

    aapt2: Update wording of sparse encoding flags
    Test: `m aapt2`
    Bug: 452370194
    Flag: EXEMPT HOST_MODE
    Change-Id: Ib1581bbe2efb552f36d72da2b6f0b4b3b4c489e7

--- tools/aapt2/cmd/Link.h
+++ tools/aapt2/cmd/Link.h
@@ -167,7 +167,8 @@ class LinkCommand : public Command {
     AddOptionalSwitch(
         "--enable-sparse-encoding",
         "Enables encoding sparse entries using a binary search tree.\n"
-        "This decreases APK size at the cost of resource retrieval performance.\n"
+        "This leads to a reduction in APK size, memory usage, and startup latency, and a small "
+        "increase in individual resource lookup time after startup.\n"
         "Only applies sparse encoding if minSdk of the APK is >= 32",
         &options_.use_sparse_encoding);
     AddOptionalSwitch("--enable-compact-entries",
