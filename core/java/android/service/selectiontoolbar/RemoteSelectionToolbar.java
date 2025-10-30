114e5d4bc4d3155b692d29ec4a8f3ef86e42dd91
Evan Severson <evanseverson@google.com>
2025-10-30 11:47:24 -0700

    Respect left side system bars in RemoteSelectionToolbar
    These changes weren't carried over when migrating from local toolbar to
    the remote toolbar.
    
    This combines the following changes in the RemoteSelectionToolbar:
    I81be5f6d32fb0ac318f1efc81b776824e0951427
    I277376b40de8ad17c006b82f2160a665ccdc38a2
    
    Test: Manual
    Fixes: 454975571
    Flag: EXEMPT bugfix
    Change-Id: I4f93efd4ba5c4415d068a32ef92b68ee2896b983

--- core/java/android/service/selectiontoolbar/RemoteSelectionToolbar.java
+++ core/java/android/service/selectiontoolbar/RemoteSelectionToolbar.java
@@ -435,11 +435,15 @@ public final class RemoteSelectionToolbar {
     }
 
     private void refreshCoordinatesAndOverflowDirection(Rect contentRectOnScreen) {
-        // Initialize x ensuring that the toolbar isn't rendered behind the nav bar in
-        // landscape.
-        final int x = Math.min(
-                contentRectOnScreen.centerX() - mPopupWidth / 2,
-                mViewPortOnScreen.right - mPopupWidth);
+        final int x;
+        if (mPopupWidth > mViewPortOnScreen.width()) {
+            // Not enough space - prefer to position as far left as possible
+            x = mViewPortOnScreen.left;
+        } else {
+            // Initialize x ensuring that the toolbar isn't rendered behind the system bar insets
+            x = Math.clamp(contentRectOnScreen.centerX() - mPopupWidth / 2,
+                    mViewPortOnScreen.left, mViewPortOnScreen.right - mPopupWidth);
+        }
 
         final int y;
 
