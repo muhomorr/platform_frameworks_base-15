7b5f9a6afd78abaeb3cefb8b775b0499d3b91e95
Sergey Pinkevich <spinkevich@google.com>
2025-11-04 13:31:10 +0000

    Flag removal: enable_desktop_opening_deeplink_minimize_animation_bugfix
    Bug: 410817470
    Flag: EXEMPT FLAG_REMOVAL
    Test: atest WMShellUnitTests:DesktopMixedTransitionHandlerTest
    
    Change-Id: Ic55ea6a4ee057299f819d4294c70272460545cdc

--- core/java/android/window/DesktopModeFlags.java
+++ core/java/android/window/DesktopModeFlags.java
@@ -56,8 +56,6 @@ public enum DesktopModeFlags {
     ENABLE_DESKTOP_IMMERSIVE_DRAG_BUGFIX(Flags::enableDesktopImmersiveDragBugfix, true),
     ENABLE_DESKTOP_INDICATOR_IN_SEPARATE_THREAD_BUGFIX(
             Flags::enableDesktopIndicatorInSeparateThreadBugfix, true),
-    ENABLE_DESKTOP_OPENING_DEEPLINK_MINIMIZE_ANIMATION_BUGFIX(
-            Flags::enableDesktopOpeningDeeplinkMinimizeAnimationBugfix, true),
     ENABLE_DESKTOP_TAB_TEARING_MINIMIZE_ANIMATION_BUGFIX(
             Flags::enableDesktopTabTearingMinimizeAnimationBugfix, true),
     ENABLE_DESKTOP_WALLPAPER_ACTIVITY_FOR_SYSTEM_USER(
