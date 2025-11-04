1d68a1099be2b99e8410dad01822851287994682
Hiroki Sato <hirokisato@google.com>
2025-11-04 17:29:42 +0900

    Introduce InputMethodSubtypeSafeList
    IMM#getEnabledInputMethodSubtypeList() can return a large list of
    subtypes, which may cause a TransactionTooLargeException.
    
    This patch introduces InputMethodSubtypeSafeList to wrap the list as a
    byte array, avoiding the exception. This mirrors the existing
    InputMethodInfoSafeList pattern introduced in [1].
    
    Additionally, this change extracts the common marshalling logic from
    InputMethodInfoSafeList into a new AbstractSafeList and refactors both
    SafeList classes to extend it.
    
    [1] I0a7667070fcdf17d34b248a5988c38064588718a
    
    Bug: 449416164
    Bug: 449181366
    Bug: 449393786
    Bug: 449227003
    Test: CtsInputMethodTestCases:{InputMethodRegistrationTest,InputMethodInfoTest}
    Test: InputMethodCoreTests
    Flag: EXEMPT BUGFIX
    Change-Id: Ied64a9f018fd3e79cfc51ccd82d361b43e5f29dc

--- services/core/java/com/android/server/inputmethod/IInputMethodManagerImpl.java
+++ services/core/java/com/android/server/inputmethod/IInputMethodManagerImpl.java
@@ -47,6 +47,7 @@ import com.android.internal.inputmethod.IRemoteAccessibilityInputConnection;
 import com.android.internal.inputmethod.IRemoteComputerControlInputConnection;
 import com.android.internal.inputmethod.IRemoteInputConnection;
 import com.android.internal.inputmethod.InputMethodInfoSafeList;
+import com.android.internal.inputmethod.InputMethodSubtypeSafeList;
 import com.android.internal.inputmethod.StartInputFlags;
 import com.android.internal.inputmethod.StartInputReason;
 import com.android.internal.view.IInputMethodManager;
@@ -99,7 +100,8 @@ final class IInputMethodManagerImpl extends IInputMethodManager.Stub {
         @NonNull
         InputMethodInfoSafeList getEnabledInputMethodList(@UserIdInt int userId);
 
-        List<InputMethodSubtype> getEnabledInputMethodSubtypeList(String imiId,
+        @NonNull
+        InputMethodSubtypeSafeList getEnabledInputMethodSubtypeList(String imiId,
                 boolean allowsImplicitlyEnabledSubtypes, @UserIdInt int userId);
 
         InputMethodSubtype getLastInputMethodSubtype(@UserIdInt int userId);
@@ -260,8 +262,9 @@ final class IInputMethodManagerImpl extends IInputMethodManager.Stub {
         return mCallback.getEnabledInputMethodList(userId);
     }
 
+    @NonNull
     @Override
-    public List<InputMethodSubtype> getEnabledInputMethodSubtypeList(String imiId,
+    public InputMethodSubtypeSafeList getEnabledInputMethodSubtypeList(String imiId,
             boolean allowsImplicitlyEnabledSubtypes, @UserIdInt int userId) {
         return mCallback.getEnabledInputMethodSubtypeList(imiId, allowsImplicitlyEnabledSubtypes,
                 userId);
