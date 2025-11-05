595cf99ecd42927eebf804638a4623313f3f14db
Nate Myren <ntmyren@google.com>
2025-11-05 14:36:49 -0800

    Trim permission, permission group names
    Bug: 453649815
    Test: atest AppSecurityTests
    Flag: EXEMPT CVE_FIX
    Change-Id: I673ad83d05c9825177967e4f0a960e8841610b71

--- core/java/com/android/internal/pm/pkg/component/ParsedPermissionUtils.java
+++ core/java/com/android/internal/pm/pkg/component/ParsedPermissionUtils.java
@@ -182,6 +182,8 @@ public class ParsedPermissionUtils {
             }
         }
 
+        permission.setName(permission.getName().trim());
+
         permission.setProtectionLevel(
                 PermissionInfo.fixProtectionLevel(permission.getProtectionLevel()));
 
@@ -263,6 +265,8 @@ public class ParsedPermissionUtils {
             sa.recycle();
         }
 
+        permission.setName(permission.getName().trim());
+
         int index = permission.getName().indexOf('.');
         if (index > 0) {
             index = permission.getName().indexOf('.', index + 1);
@@ -312,7 +316,8 @@ public class ParsedPermissionUtils {
                     .setBackgroundRequestDetailRes(sa.getResourceId(R.styleable.AndroidManifestPermissionGroup_backgroundRequestDetail, 0))
                     .setRequestRes(sa.getResourceId(R.styleable.AndroidManifestPermissionGroup_request, 0))
                     .setPriority(sa.getInt(R.styleable.AndroidManifestPermissionGroup_priority, 0))
-                    .setFlags(sa.getInt(R.styleable.AndroidManifestPermissionGroup_permissionGroupFlags,0));
+                    .setFlags(sa.getInt(R.styleable.AndroidManifestPermissionGroup_permissionGroupFlags,0))
+                    .setName(permissionGroup.getName().trim());
             // @formatter:on
         } finally {
             sa.recycle();
