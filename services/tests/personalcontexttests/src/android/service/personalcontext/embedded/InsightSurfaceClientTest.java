1bff7dc48929670d361d5cbe1f8d22c6a02cca7a
Will Leshner <wleshner@google.com>
2025-11-13 10:39:07 -0800

    Introduce a "register/onSurfaceCreated" API for InsightSurfaceClient.
    InsightSurfaceClient will no longer manage a SurfaceView directly.
    Instead, it will offer a register() API and then be notified when a
    surface is ready with an onSurfaceCreated() callback.
    
    Bug: 434644900
    Bug: 461071414
    Test: atest InsightSurfaceClientTest
    Flag: android.service.personalcontext.enable_personal_context_service
    API-Coverage-Bug: 452773336
    Change-Id: I08cb6fd69f18fd25736f082ee86004dc439b894d

---
+++ services/tests/personalcontexttests/src/android/service/personalcontext/embedded/InsightSurfaceClientTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.service.personalcontext.embedded;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.RemoteException;
+import android.service.personalcontext.PersonalContextManager;
+import android.service.personalcontext.hint.BundleHint;
+import android.view.Display;
+import android.view.SurfaceControlViewHost.SurfacePackage;
+
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+@SmallTest
+@RunWith(AndroidJUnit4.class)
+public class InsightSurfaceClientTest {
+    @Mock private Context mContext;
+    @Mock private SurfacePackage mSurfacePackage;
+    @Mock private PersonalContextManager mPersonalContextManager;
+    @Mock private InsightSurfaceClient.ClientCallback mConnectionCallbacks;
+    @Mock private Display mDisplay;
+    @Mock private Resources mResources;
+    @Mock private Configuration mConfiguration;
+    private Executor mExecutor = Runnable::run;
+    private BundleHint mHint = new BundleHint();
+    private InsightSurfaceClient mClient;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+
+        when(mContext.getSystemService(PersonalContextManager.class))
+                .thenReturn(mPersonalContextManager);
+        when(mContext.getDisplay()).thenReturn(mDisplay);
+        when(mContext.getResources()).thenReturn(mResources);
+        when(mResources.getConfiguration()).thenReturn(mConfiguration);
+
+        mClient =
+                new InsightSurfaceClient.Builder(mContext, mExecutor, mConnectionCallbacks)
+                        .addHint(mHint)
+                        .build();
+    }
+
+    @Test
+    public void testSurfaceCreated() throws RemoteException {
+        mClient.register(0, 0);
+        final ArgumentCaptor<InsightSurfaceClientInfo> clientInfoCaptor =
+                ArgumentCaptor.forClass(InsightSurfaceClientInfo.class);
+        verify(mPersonalContextManager).registerInsightSurfaceClient(
+                clientInfoCaptor.capture(), eq(List.of(mHint)));
+
+        final IEmbeddedInsightSurfaceCallback callback = clientInfoCaptor.getValue().getCallback();
+        callback.onSurfaceCreated(mSurfacePackage);
+        verify(mConnectionCallbacks).onSurfaceCreated(mSurfacePackage);
+    }
+
+    @Test
+    public void testSurfaceReleased() throws RemoteException {
+        mClient.register(0, 0);
+        mClient.unregister();
+        final ArgumentCaptor<InsightSurfaceClientInfo> clientInfoCaptor =
+                ArgumentCaptor.forClass(InsightSurfaceClientInfo.class);
+        verify(mPersonalContextManager).unregisterInsightSurfaceClient(clientInfoCaptor.capture());
+
+        final IEmbeddedInsightSurfaceCallback callback = clientInfoCaptor.getValue().getCallback();
+        callback.onSurfaceReleased(mSurfacePackage);
+        verify(mConnectionCallbacks).onSurfaceReleased(mSurfacePackage);
+    }
+}
