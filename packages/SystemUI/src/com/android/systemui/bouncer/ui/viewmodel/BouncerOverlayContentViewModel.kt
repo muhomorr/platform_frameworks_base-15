ec49d580e39da973af46ff84bc7f05bea1b7e7cd
Alexander Hendrich <hendrich@google.com>
2025-11-20 01:48:26 -0800

    Move BouncerInputSide to bouncer shared models
    BouncerInputSide better fits to the bouncer shared models than the authentication shared models. This became obvious because the other authentication shared models would likely be moved together into the shared authentication library that we're planning (more details to follow soon), whereas BouncerInputSide would not.
    
    Bug: None
    Flag: EXEMPT PURE_REFACTOR
    Test: None (trivial refactor)
    Change-Id: I3c6656de67eaca749291380bca412f9be6858bc8

--- packages/SystemUI/src/com/android/systemui/bouncer/ui/viewmodel/BouncerOverlayContentViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/bouncer/ui/viewmodel/BouncerOverlayContentViewModel.kt
@@ -34,10 +34,10 @@ import com.android.systemui.Flags
 import com.android.systemui.authentication.domain.interactor.AuthenticationInteractor
 import com.android.systemui.authentication.shared.model.AuthenticationMethodModel
 import com.android.systemui.authentication.shared.model.AuthenticationWipeModel
-import com.android.systemui.authentication.shared.model.BouncerInputSide
 import com.android.systemui.bouncer.domain.interactor.BouncerActionButtonInteractor
 import com.android.systemui.bouncer.domain.interactor.BouncerInteractor
 import com.android.systemui.bouncer.shared.model.BouncerActionButtonModel
+import com.android.systemui.bouncer.shared.model.BouncerInputSide
 import com.android.systemui.bouncer.ui.BouncerColors.surfaceColor
 import com.android.systemui.bouncer.ui.helper.BouncerHapticPlayer
 import com.android.systemui.common.shared.model.Icon
