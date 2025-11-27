256e7694a789af87cf6d7637f49eaba03b8137b5
Mayank Agarwal <emayank@google.com>
2025-11-27 20:13:15 +0000

    Log atom for screen state considering all displays
    For multi display devices we would like to log ON if even one of the
    devices is ON, and so on for other state of DOZE etc.
    This change aims to address both latency and correctness issues that
    surfaced previously while attempting the solution before.
    
    Flag: com.android.server.am.battery_stats_log_combined_display_state
    Test: atest BatteryStatsServiceAtomTest and manually on
    multi display foldable Pixel Felix. Tested with and
    without the flag and noticed desired change especially going from closed
    to open inner display where atom still showed up as ON even when outer
    display turned off.
    Bug: 416368479
    
    Change-Id: I305bf029ff9b99d35b76ff16c087b05a2768486c

--- services/core/java/com/android/server/am/BatteryStatsService.java
+++ services/core/java/com/android/server/am/BatteryStatsService.java
@@ -100,6 +100,7 @@ import android.util.AtomicFile;
 import android.util.IndentingPrintWriter;
 import android.util.Slog;
 import android.util.StatsEvent;
+import android.view.Display;
 
 import com.android.internal.R;
 import com.android.internal.annotations.GuardedBy;
@@ -192,6 +193,10 @@ public final class BatteryStatsService extends IBatteryStats.Stub
     private final PowerStatsUidResolver mPowerStatsUidResolver = new PowerStatsUidResolver();
     private final PowerAttributor mPowerAttributor;
     private final PowerManagerFlags mPowerManagerFlags = new PowerManagerFlags();
+
+    private final Object mDisplayStatesLock = new Object();
+    @GuardedBy("mDisplayStatesLock")
+    private int[] mDisplayStates;
 
     private volatile boolean mMonitorEnabled = true;
     private boolean mRailsStatsCollectionEnabled = true;
@@ -389,6 +394,9 @@ public final class BatteryStatsService extends IBatteryStats.Stub
         mStats = new BatteryStatsImpl(mBatteryStatsConfig, Clock.SYSTEM_CLOCK, mMonotonicClock,
                 systemDir, mHandler, this, this, mUserManagerUserInfoProvider, mPowerProfile,
                 mCpuScalingPolicies, mPowerStatsUidResolver);
+        if (mStats.getDisplayCount() > 0) {
+            mDisplayStates = new int[mStats.getDisplayCount()];
+        }
         mWorker = new BatteryExternalStatsWorker(context, mStats, mHandler, mClock);
         mStats.setExternalStatsSyncLocked(mWorker);
         mStats.setRadioScanningTimeoutLocked(mContext.getResources().getInteger(
@@ -687,6 +695,23 @@ public final class BatteryStatsService extends IBatteryStats.Stub
             } catch (InterruptedException e) {
                 // Keep looping
             }
+        }
+    }
+
+    /**
+     * Sets the number of displays to track in BatteryStats. Mainly for testing.
+     *
+     * @param displayCount The number of displays.
+     * @hide
+     */
+    // TODO(b/467244575) : Use stricter checks to restrict for test usage only.
+    @VisibleForTesting
+    void setDisplayCount(final int displayCount) {
+        synchronized (mDisplayStatesLock) {
+            mDisplayStates = new int[displayCount];
+        }
+        synchronized (mStats) {
+            mStats.setDisplayCountLocked(displayCount);
         }
     }
 
@@ -1785,7 +1810,34 @@ public final class BatteryStatsService extends IBatteryStats.Stub
     @EnforcePermission(UPDATE_DEVICE_STATS)
     public void noteScreenState(final int displayId, final int state, final int reason) {
         super.noteScreenState_enforcePermission();
-        FrameworkStatsLog.write(FrameworkStatsLog.SCREEN_STATE_CHANGED, state);
+        if (com.android.server.am.Flags.batteryStatsLogCombinedDisplayState()) {
+            int combinedState = Display.STATE_UNKNOWN;
+            synchronized (mDisplayStatesLock) {
+                if (mDisplayStates == null) {
+                    Slog.w(TAG, "noteScreenState called with displayId: " + displayId
+                            + ", but mDisplayStates is null.");
+                    return;
+                }
+                if (displayId < 0 || displayId >= mDisplayStates.length) {
+                    Slog.w(TAG, "noteScreenState called with out of bounds displayId: " + displayId
+                            + ", mDisplayStates.length=" + mDisplayStates.length);
+                    return;
+                }
+                mDisplayStates[displayId] = BatteryStatsImpl.getSanitizedDisplayState(state);
+                combinedState = mDisplayStates[displayId];
+                int currentPriority = BatteryStatsImpl.getDisplayStatePriority(combinedState);
+                for (int i = 0; i < mDisplayStates.length; i++) {
+                    if (BatteryStatsImpl.getDisplayStatePriority(mDisplayStates[i])
+                            > currentPriority) {
+                        combinedState = mDisplayStates[i];
+                        currentPriority = BatteryStatsImpl.getDisplayStatePriority(combinedState);
+                    }
+                }
+            }
+            FrameworkStatsLog.write(FrameworkStatsLog.SCREEN_STATE_CHANGED, combinedState);
+        } else {
+            FrameworkStatsLog.write(FrameworkStatsLog.SCREEN_STATE_CHANGED, state);
+        }
 
         synchronized (mClock) {
             final long elapsedRealtime = mClock.elapsedRealtime();
