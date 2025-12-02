2d236b1cbe9871a916239c318aa50397cabc6ad2
Achim Thesmann <achim@google.com>
2025-12-02 21:22:27 +0000

    Allow ApplicationThread usage from Shell
    This is a followup to ag/37026730 (AI generated flag removal), which slightly changed the logic, which prevents shell access.
    We still log the potential violation, but won't stop shell from accessing the ApplicationThread (for now).
    
    This also:
    - introduces a shortcut to determine the package name for SHELL_UID
    - logs the behavior as shell is logged, but does not fail as other UIDs
    - removes stream API usage (in the rare case of shared UIDs the package name is not critical)
    
    None of this restricts the current behavior, only allows shell to access the ApplicationThread.
    
    Bug: 463990269
    Test: atest ActivityThreadTest BackgroundActivityLaunchTest
    Flag: EXEMPT BUGFIX
    Change-Id: Ia6c728e328476197a61d977be779b1190b25e3ae

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -302,7 +302,6 @@ import java.util.TimeZone;
 import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 /**
  * This manages the execution of the main thread in an
@@ -1185,27 +1184,37 @@ public final class ActivityThread extends ClientTransactionHandler
                 throws RemoteException {
             int callingUid = Binder.getCallingUid();
             if (callingUid != Process.ROOT_UID && callingUid != Process.SYSTEM_UID) {
-                String[] packagesForUid =
-                        getSystemContext().getPackageManager().getPackagesForUid(callingUid);
                 String packageName;
-                if (packagesForUid == null || packagesForUid.length == 0) {
-                    packageName = "unknown";
-                } else if (packagesForUid.length == 1) {
-                    packageName = packagesForUid[0];
+                if (callingUid == Process.SHELL_UID) {
+                    // save lookup, we know and expect this
+                    packageName = "com.android.shell";
                 } else {
-                    packageName = Arrays.asList(packagesForUid).stream().sorted().collect(
-                            Collectors.joining(", "));
+                    String[] packagesForUid = getPackageManager().getPackagesForUid(callingUid);
+                    if (packagesForUid == null || packagesForUid.length == 0) {
+                        packageName = "unknown";
+                    } else if (packagesForUid.length == 1) {
+                        packageName = packagesForUid[0];
+                    } else {
+                        // shared UID - just use first package name and annotate it
+                        packageName = packagesForUid[0] + "*";
+                    }
                 }
-                Slog.wtf(TAG, "ApplicationThread called by non-system process"
-                        + " (callingUid: " + callingUid
-                        + "; packageName: " + packageName
-                        + "; code: " + code
-                        + "; flags: " + flags
-                        + ")");
-                throw new SecurityException(
-                        "ApplicationThread called by non-system process"
-                                + " (callingUid: " + callingUid
-                                + "; packageName: " + packageName + ")");
+                boolean fail = callingUid != Process.SHELL_UID;
+                if (Build.IS_DEBUGGABLE || fail) {
+                    Slog.wtf(TAG, "ApplicationThread called by non-system process"
+                            + " (callingUid: " + callingUid
+                            + "; packageName: " + packageName
+                            + "; code: " + code
+                            + "; flags: " + flags
+                            + "; behavior: " + (fail ? "fail" : "log")
+                            + ")");
+                }
+                if (fail) {
+                    throw new SecurityException(
+                            "ApplicationThread called by non-system process"
+                                    + " (callingUid: " + callingUid
+                                    + "; packageName: " + packageName + ")");
+                }
             }
             return super.onTransact(code, data, reply, flags);
         }
