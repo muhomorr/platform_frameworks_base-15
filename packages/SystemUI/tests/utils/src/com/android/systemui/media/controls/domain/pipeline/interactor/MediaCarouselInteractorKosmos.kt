3629d0d7e1afe2d0f05e4e4ddec3a1f8b1cb9af8
Michael Mikhail <michaelmikhil@google.com>
2025-12-04 15:40:41 +0000

    [Flexiglass] Add media UIEvent logs
    This CL adds logs for all UiEvents needed except actions and locations.
    
    Flag: com.android.systemui.scene_container
    Bug: 312717496
    Test: Checked logs.
    Change-Id: I9f4aa53003a06b20e524f4f0a3a7915dc52d7a80

--- packages/SystemUI/tests/utils/src/com/android/systemui/media/controls/domain/pipeline/interactor/MediaCarouselInteractorKosmos.kt
+++ packages/SystemUI/tests/utils/src/com/android/systemui/media/controls/domain/pipeline/interactor/MediaCarouselInteractorKosmos.kt
@@ -27,6 +27,7 @@ import com.android.systemui.media.controls.domain.pipeline.mediaDeviceManager
 import com.android.systemui.media.controls.domain.pipeline.mediaResumeListener
 import com.android.systemui.media.controls.domain.pipeline.mediaSessionBasedFilter
 import com.android.systemui.media.controls.domain.pipeline.mediaTimeoutListener
+import com.android.systemui.media.controls.util.mediaUiEventLogger
 import com.android.systemui.media.remedia.data.repository.mediaPipelineRepository
 import com.android.systemui.media.remedia.data.repository.mediaRepository
 
@@ -43,6 +44,7 @@ val Kosmos.mediaCarouselInteractor by
             mediaDataFilter = mediaDataFilter,
             mediaPipelineRepository = mediaPipelineRepository,
             mediaRepository = mediaRepository,
+            mediaUiEventLogger = mediaUiEventLogger,
             keyguardTransitionInteractor = keyguardTransitionInteractor,
             deviceEntryInteractor = deviceEntryInteractor,
         )
