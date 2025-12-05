77dae840355dd8dca14d063736bc6409ddb93835
Gustav Sennton <gsennton@google.com>
2025-12-05 03:56:36 -0800

    Run fundamentals/enter tests on emulators
    (the tests will still run on physical devices as well - in postsubmit)
    
    For each test:
    1. Annotate as @Postsubmit to not break presubmit
    2. Remove @RequiresDesktopDevice annotation
    3. Remove "device" test_mapping keyword
    
    Bug: 455471041
    Flag: EXEMPT TEST_ONLY
    Test: N/A
    Change-Id: I0270c080afac366b3a60ca13da44c47d6981504d

--- libs/WindowManager/Shell/tests/e2e/desktopmode/scenarios/src/com/android/wm/shell/functional/fundamentals/enter/EnterSplitScreenWithAppHandleMenuTest.kt
+++ libs/WindowManager/Shell/tests/e2e/desktopmode/scenarios/src/com/android/wm/shell/functional/fundamentals/enter/EnterSplitScreenWithAppHandleMenuTest.kt
@@ -16,15 +16,13 @@
 
 package com.android.wm.shell.functional.fundamentals.enter
 
-import android.platform.test.annotations.Presubmit
-import android.platform.test.annotations.RequiresDesktopDevice
+import android.platform.test.annotations.Postsubmit
 import android.platform.test.rule.ScreenRecordRule
 import com.android.wm.shell.scenarios.EnterSplitScreenWithAppHandleMenu
 import org.junit.runner.RunWith
 import org.junit.runners.BlockJUnit4ClassRunner
 
 @RunWith(BlockJUnit4ClassRunner::class)
-@RequiresDesktopDevice
-@Presubmit
+@Postsubmit
 @ScreenRecordRule.ScreenRecord
 class EnterSplitScreenWithAppHandleMenuTest : EnterSplitScreenWithAppHandleMenu()
