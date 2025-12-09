85b4c6852187fd50a6fb6e140756e2546b3093ce
Rodrigo Chaves <rchaves@google.com>
2025-12-09 06:18:10 -0800

    Use cached list of role holders to verify supervision policy API access
    Roll forward of ag/37268985
    
    
    Flag: android.app.supervision.flags.enable_supervision_manager_policy_apis
    Bug: 456763743
    Change-Id: I0272ce4e398e57f8b5d07abaf3394a184502ae8a

--- services/supervision/java/com/android/server/supervision/SupervisionService.java
+++ services/supervision/java/com/android/server/supervision/SupervisionService.java
@@ -1048,10 +1048,11 @@ public class SupervisionService extends ISupervisionManager.Stub {
         UserHandle userHandle = Binder.getCallingUserHandle();
         int callingUid = Binder.getCallingUid();
 
-        List<String> roleHolders =
-                new ArrayList<String>(
-                        mInjector.getRoleHoldersAsUser(ROLE_SYSTEM_SUPERVISION, userHandle));
-        roleHolders.addAll(mInjector.getRoleHoldersAsUser(ROLE_SUPERVISION, userHandle));
+        List<String> roleHolders = new ArrayList<String>();
+        synchronized (getLockObject()) {
+            roleHolders.addAll(
+                    getUserDataLocked(UserHandle.getUserId(callingUid)).supervisionRoleHolders);
+        }
 
         String[] packages = mInjector.getPackageManager().getPackagesForUid(callingUid);
         if (packages != null) {
@@ -1074,7 +1075,10 @@ public class SupervisionService extends ISupervisionManager.Stub {
      */
     private List<String> updateSupervisionRoleHolders(@UserIdInt int userId) {
         List<String> newRoleHolders =
-                mInjector.getRoleHoldersAsUser(ROLE_SUPERVISION, UserHandle.of(userId));
+                new ArrayList<String>(
+                        mInjector.getRoleHoldersAsUser(ROLE_SUPERVISION, UserHandle.of(userId)));
+        newRoleHolders.addAll(
+                mInjector.getRoleHoldersAsUser(ROLE_SYSTEM_SUPERVISION, UserHandle.of(userId)));
 
         synchronized (getLockObject()) {
             SupervisionUserData data = getUserDataLocked(userId);
@@ -1408,7 +1412,7 @@ public class SupervisionService extends ISupervisionManager.Stub {
 
         @Override
         public void onRoleHoldersChanged(@NonNull String roleName, @NonNull UserHandle user) {
-            if (ROLE_SUPERVISION.equals(roleName)) {
+            if (ROLE_SUPERVISION.equals(roleName) || ROLE_SYSTEM_SUPERVISION.equals(roleName)) {
                 executeOnServiceThread(
                         () -> {
                             maybeApplyUserRestrictionsFor(user);
