c28b1200444bd8aaf69064664be90b18dc707436
Chun-Ku Lin <chunkulin@google.com>
2025-12-09 10:01:56 +0000

    Create skelenton screen for top level settings screen
    Bug: 450090715
    Test: manual
    Test: atest
    Flag: com.android.systemui.accessibility.accessibilitymenu.catalyst_a11y_menu
    Change-Id: Idf375a2753a0fa58a1f055d9eb6fb46b3b772f3f

---
+++ packages/SystemUI/accessibility/accessibilitymenu/src/com/android/systemui/accessibility/accessibilitymenu/A11yMenuApplication.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.accessibility.accessibilitymenu
+
+import android.app.Application
+import com.android.settingslib.metadata.FixedArrayMap
+import com.android.settingslib.metadata.PreferenceScreenRegistry
+import com.android.systemui.accessibility.accessibilitymenu.settings.TopLevelSettingsScreen
+
+/** Application class for the Accessibility Menu. */
+class A11yMenuApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (Flags.catalystA11yMenu()) {
+            PreferenceScreenRegistry.preferenceScreenMetadataFactories =
+                FixedArrayMap(1) { it.put(TopLevelSettingsScreen.KEY) { TopLevelSettingsScreen() } }
+        }
+    }
+}
