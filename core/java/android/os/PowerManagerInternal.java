e31e4acca0d28e7c3fb89628f8f436713bee1656
Fiona Campbell <flc@google.com>
2025-12-11 18:02:45 +0000

    Wake up adb command for powermanager
    Bug: 450533343
    Flag: EXEMPT DEBUG
    Test: adb shell cmd power wakeup 3000 --group-id 0 2 --display-id 1
    
    Change-Id: Idf7a3fd1a0aeaecf637a4910a6b3028f36868f95

--- core/java/android/os/PowerManagerInternal.java
+++ core/java/android/os/PowerManagerInternal.java
@@ -316,11 +316,25 @@ public abstract class PowerManagerInternal {
      *
      * @param groupIds power groups that should be put to sleep
      * @param eventTime when the request was issued
-     * @param reason reason for going to sleep
+     * @param reason reason for going to sleep - any of
+     * {@link android.os.PowerManager.GoToSleepReason}
      * @param flags PowerManager sleep flags
      */
     public abstract void goToSleepPerGroup(IntArray groupIds, long eventTime, int reason,
             int flags);
+
+    /**
+     * Used to wake up certain power groups.
+     *
+     * @param groupIds Power groups that should be woken up
+     * @param eventTime When the request was issued
+     * @param reason Reason for waking - any of {@link android.os.PowerManager.WakeReason}
+     * @param details Details about the event.
+     * @param opPackageName The Package name used for AppOps.
+     * @param uid The uid used for AppOps.
+     */
+    public abstract void wakeupPerGroup(IntArray groupIds, long eventTime, int reason,
+            String details, String opPackageName, int uid);
 
     /**
      * Boost: It is sent when user interacting with the device, for example,
