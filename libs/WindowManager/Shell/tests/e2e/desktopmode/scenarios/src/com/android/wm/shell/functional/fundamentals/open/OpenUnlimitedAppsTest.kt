1368b9a709e9d806a4cbc7b98c8870efa743e09e
Gustav Sennton <gsennton@google.com>
2025-12-11 08:28:04 -0800

    Run fundamentals/open tests on emulators
    For each test:
    1. Annotate as @Postsubmit to not break presubmit
    2. Remove @RequiresDesktopDevice annotation
    3. Remove "device" test_mapping keyword
    
    Bug: 455471041
    Flag: EXEMPT TEST_ONLY
    Test: N/A
    Change-Id: Id72aaf18b67689466b27c3fec056d9923bcf1eae

--- libs/WindowManager/Shell/tests/e2e/desktopmode/scenarios/src/com/android/wm/shell/functional/fundamentals/open/OpenUnlimitedAppsTest.kt
+++ libs/WindowManager/Shell/tests/e2e/desktopmode/scenarios/src/com/android/wm/shell/functional/fundamentals/open/OpenUnlimitedAppsTest.kt
@@ -16,8 +16,7 @@
 
 package com.android.wm.shell.functional.fundamentals.open
 
-import android.platform.test.annotations.Presubmit
-import android.platform.test.annotations.RequiresDesktopDevice
+import android.platform.test.annotations.Postsubmit
 import android.platform.test.rule.ScreenRecordRule
 import com.android.wm.shell.scenarios.OpenUnlimitedApps
 import org.junit.runner.RunWith
@@ -25,7 +24,6 @@ import org.junit.runners.BlockJUnit4ClassRunner
 
 /* Functional test for [OpenUnlimitedApps]. */
 @RunWith(BlockJUnit4ClassRunner::class)
-@RequiresDesktopDevice
-@Presubmit
+@Postsubmit
 @ScreenRecordRule.ScreenRecord
 class OpenUnlimitedAppsTest : OpenUnlimitedApps()
