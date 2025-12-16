0ddcccfd30d620bf99c45755146a44483b1e67d8
Diya Bera <diyab@google.com>
2025-12-16 19:16:46 +0000

    Remove hidl_service_died_fix flag
    Flag: EXEMPT FLAG_REMOVAL
    Test: N/A
    Fixes: 464972673
    Change-Id: I0a513b7ac16a7d0066eecc6473bfa84afc9a8f82

--- services/tests/servicestests/src/com/android/server/biometrics/sensors/fingerprint/hidl/HidlToAidlSensorAdapterTest.java
+++ services/tests/servicestests/src/com/android/server/biometrics/sensors/fingerprint/hidl/HidlToAidlSensorAdapterTest.java
@@ -41,7 +41,6 @@ import android.os.RemoteException;
 import android.os.UserHandle;
 import android.os.test.TestLooper;
 import android.platform.test.annotations.Presubmit;
-import android.platform.test.annotations.RequiresFlagsEnabled;
 import android.platform.test.flag.junit.CheckFlagsRule;
 import android.platform.test.flag.junit.DeviceFlagsValueProvider;
 import android.testing.TestableContext;
@@ -49,7 +48,6 @@ import android.testing.TestableContext;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.filters.SmallTest;
 
-import com.android.server.biometrics.Flags;
 import com.android.server.biometrics.log.BiometricContext;
 import com.android.server.biometrics.log.BiometricLogger;
 import com.android.server.biometrics.sensors.AuthSessionCoordinator;
@@ -234,7 +232,6 @@ public class HidlToAidlSensorAdapterTest {
     }
 
     @Test
-    @RequiresFlagsEnabled(Flags.FLAG_HIDL_SERVICE_DIED_FIX)
     public void serviceDiedTest() {
         mHidlToAidlSensorAdapter.setScheduler(mBiometricScheduler);
         mHidlToAidlSensorAdapter.serviceDied(0 /* cookie */);
