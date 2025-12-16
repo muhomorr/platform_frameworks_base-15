74f13bfcdb1728f8dbc753f2725f4e2da174eb15
Chris Li <lihongyu@google.com>
2025-12-16 14:18:40 +0800

    Update flicker animation bounds test to verify screenBounds
    screenBounds can catch the failure where the scaling changes in
    direction.
    
    Also mark the new test cases as flaky until resolve the bubble switch
    issue.
    
    Bug: 456051408
    Test: atest WMShellExplicitFlickerTestsBubbles
    Flag: EXEMPT TEST_ONLY
    Change-Id: I7779fac58523623838acb6d94bf845d29d581d2f

--- libs/WindowManager/Shell/tests/e2e/bubbles/flicker-explicit/src/com/android/wm/shell/flicker/bubbles/utils/FlickerAssertionHelper.kt
+++ libs/WindowManager/Shell/tests/e2e/bubbles/flicker-explicit/src/com/android/wm/shell/flicker/bubbles/utils/FlickerAssertionHelper.kt
@@ -18,14 +18,20 @@ package com.android.wm.shell.flicker.bubbles.utils
 
 import android.tools.flicker.subject.layers.LayersTraceSubject
 import android.tools.traces.component.IComponentNameMatcher
+import kotlin.math.roundToInt
 
 /** A helper to assert the flicker traces. */
 internal object FlickerAssertionHelper {
 
     /**
-     * Asserts the [layerMatcher]'s layer alpha value only changes in one direction when visible.
+     * Asserts the [layerMatcher]'s layer alpha value only increases or decreases when visible.
+     *
+     * Note: It is optional if the spec wants to keep alpha unchanged.
+     *
+     * @param isFadeIn if `true`, it can only increase or stay unchanged; otherwise, it can only
+     *   decrease or stay unchanged.
      */
-    fun assertLayerAlphaChange(
+    fun assertLayerAlphaChangeConsistently(
         layersTraceSubject: LayersTraceSubject,
         layerMatcher: IComponentNameMatcher,
         isFadeIn: Boolean,
@@ -49,8 +55,53 @@ internal object FlickerAssertionHelper {
         }
     }
 
-    /** Asserts the [layerMatcher]'s layer position only changes in one direction when visible. */
-    fun assertLayerPositionChange(
+    /**
+     * Asserts the [layerMatcher]'s layer screen bounds only expand or shrink when visible.
+     *
+     * Note: It is optional if the spec wants to keep bounds unchanged.
+     */
+    fun assertLayerResizeConsistently(
+        layersTraceSubject: LayersTraceSubject,
+        layerMatcher: IComponentNameMatcher,
+    ) {
+        val layerList =
+            layersTraceSubject.layers { layerMatcher.layerMatchesAnyOf(it) && it.isVisible }
+        var prevWidthChange = 0
+        var prevHeightChange = 0
+        layerList.zipWithNext { previous, current ->
+            // Use screenBounds to take scaling into account.
+            val prevBounds = previous.layer.screenBounds
+            val currBounds = current.layer.screenBounds
+            val widthChange =
+                currBounds.width().roundToInt().compareTo(prevBounds.width().roundToInt())
+            val heightChange =
+                currBounds.height().roundToInt().compareTo(prevBounds.height().roundToInt())
+            if (prevWidthChange == 0) {
+                prevWidthChange = widthChange
+            }
+            if (prevHeightChange == 0) {
+                prevHeightChange = heightChange
+            }
+
+            check(
+                (widthChange == 0 || prevWidthChange == widthChange) &&
+                    (heightChange == 0 || prevHeightChange == heightChange)
+            ) {
+                "Screen Bounds of $layerMatcher should only expand or shrink, but it has" +
+                    " prevWidthChange=$prevWidthChange currWidthChange=$widthChange" +
+                    " prevHeightChange=$prevHeightChange currHeightChange=$heightChange" +
+                    " at ${current.timestamp}"
+            }
+        }
+    }
+
+    /**
+     * Asserts the [layerMatcher]'s layer position only moves in one direction (no jumping around)
+     * when visible.
+     *
+     * Note: It is optional if the spec wants to keep bounds unchanged.
+     */
+    fun assertLayerMoveInSingleDirection(
         layersTraceSubject: LayersTraceSubject,
         layerMatcher: IComponentNameMatcher,
     ) {
@@ -59,12 +110,11 @@ internal object FlickerAssertionHelper {
         var prevXDirection = 0
         var prevYDirection = 0
         layerList.zipWithNext { previous, current ->
-            val prevX = previous.layer.bounds.left
-            val prevY = previous.layer.bounds.top
-            val currX = current.layer.bounds.left
-            val currY = current.layer.bounds.top
-            val xDirection = currX.compareTo(prevX)
-            val yDirection = currY.compareTo(prevY)
+            // Use bounds' (left, top) for position.
+            val prevBounds = previous.layer.bounds
+            val currBounds = current.layer.bounds
+            val xDirection = currBounds.left.roundToInt().compareTo(prevBounds.left.roundToInt())
+            val yDirection = currBounds.top.roundToInt().compareTo(prevBounds.top.roundToInt())
             if (prevXDirection == 0) {
                 prevXDirection = xDirection
             }
@@ -75,9 +125,9 @@ internal object FlickerAssertionHelper {
                 (xDirection == 0 || prevXDirection == xDirection) &&
                     (yDirection == 0 || prevYDirection == yDirection)
             ) {
-                "Position of $layerMatcher should only move in one direction at" +
-                    " prevXDirection=$prevXDirection prevYDirection=$prevYDirection" +
-                    " currXDirection=$xDirection currYDirection=$yDirection" +
+                "Position of $layerMatcher should only move in one direction, but it has" +
+                    " prevXDirection=$prevXDirection currXDirection=$xDirection" +
+                    " prevYDirection=$prevYDirection currYDirection=$yDirection" +
                     " at ${current.timestamp}"
             }
         }
