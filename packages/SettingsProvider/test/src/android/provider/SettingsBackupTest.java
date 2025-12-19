a042201a8def34e8bf3e2d5f96c4bafdc55fe2a6
Weilin Wu <wlogsky@google.com>
2025-12-19 19:58:28 +0800

    Add Setting key WEAR_CHARGING_EXPERIENCE_ENABLED
    The setting key is used to determine whether wear charging experience is
    enabled and dedicated brightness curve and timeout is used.
    
    Bug: 421103840
    Flag: com.android.server.display.feature.flags.auto_brightness_mode_charging
    Test: Manual test
    Change-Id: Ie59f78de3455821daf6410258289f805e2028f29
    Signed-off-by: Weilin Wu <wlogsky@google.com>

--- packages/SettingsProvider/test/src/android/provider/SettingsBackupTest.java
+++ packages/SettingsProvider/test/src/android/provider/SettingsBackupTest.java
@@ -618,6 +618,7 @@ public class SettingsBackupTest {
                     Settings.Global.Wearable.COOLDOWN_MODE_ON,
                     Settings.Global.Wearable.BEDTIME_MODE,
                     Settings.Global.Wearable.BEDTIME_HARD_MODE,
+                    Settings.Global.Wearable.WEAR_CHARGING_EXPERIENCE_ENABLED,
                     Settings.Global.Wearable.VIBRATE_FOR_ACTIVE_UNLOCK,
                     Settings.Global.Wearable.LOCK_SCREEN_STATE,
                     Settings.Global.Wearable.DISABLE_AOD_WHILE_PLUGGED,
