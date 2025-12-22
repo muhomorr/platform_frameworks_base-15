9a5bd256a505bbdf1acaa7ab7726850220c0039b
Ian Pedowitz (xWF) <ijpedowitz@google.com>
2025-12-22 05:47:14 -0800

    Revert "Refine mobile network icon in Internet Dialog (QS)"
    This reverts commit c503ef36a97c563c0e06c415a7a857041d74f71f.
    
    Reason for revert: b/470860299
    Bug: b/412105172
    Bug: 412105172
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:a213c21cfa87e5dd6e87edb0f2d5f02fba7f8283
    Merged-In: I54239b3bf95404aa8edc77e7fe8fcd9147b35c3a
    Change-Id: I54239b3bf95404aa8edc77e7fe8fcd9147b35c3a

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDetailsContentController.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDetailsContentController.java
@@ -562,7 +562,8 @@ public class InternetDetailsContentController implements AccessPointController.A
     }
 
     Drawable getSignalStrengthDrawable(int subId) {
-        Drawable drawable = getSignalStrengthDrawableNoInternet(subId);
+        Drawable drawable = mContext.getDrawable(
+                R.drawable.ic_signal_strength_zero_bar_no_internet);
         try {
             if (mTelephonyManager == null) {
                 if (DEBUG) {
@@ -589,19 +590,6 @@ public class InternetDetailsContentController implements AccessPointController.A
             e.printStackTrace();
         }
         return drawable;
-    }
-
-    Drawable getSignalStrengthDrawableNoInternet(int subId) {
-        if (!NewStatusBarIcons.isEnabled()) {
-            return mContext.getDrawable(R.drawable.ic_signal_strength_zero_bar_no_internet);
-        }
-        int numLevels = SignalStrength.NUM_SIGNAL_STRENGTH_BINS;
-        // TODO (b/470417792): Use CarrierConfigRepository instead of CarrierConfigTracker
-        if (mCarrierConfigTracker.getInflateSignalStrengthBool(subId)) {
-            numLevels++;
-        }
-        return getSignalStrengthIcon(subId, mContext, 0 /* level */, numLevels,
-                NO_CELL_DATA_TYPE_ICON, true /* cutOut */);
     }
 
     /**
