9d843256d4e1bf50169132d083b92e081cb4c18a
Caitlin Shkuratov <caitlinshk@google.com>
2025-12-22 19:42:14 +0000

    Update BinderCallOnMainThread linter to allow 2 methods of same name.
    Patchset 1 of this CL has a test I added to verify that this works, but
    I then removed it to avoid fake binder calls in the real implementation.
    
    Bug: 469073407
    Flag: TEST_ONLY
    Test: atest BinderCallOnMainThreadDetectorTest
    
    Change-Id: I89a1d6315829c79388149b01aa7bf41c9f0cc679

--- packages/SystemUI/checks/src/com/android/internal/systemui/lint/BinderCallOnMainThreadDetector.kt
+++ packages/SystemUI/checks/src/com/android/internal/systemui/lint/BinderCallOnMainThreadDetector.kt
@@ -75,19 +75,21 @@ class BinderCallOnMainThreadDetector : Detector(), SourceCodeScanner {
         }
 
     override fun getApplicableMethodNames(): List<String> =
-        parsedBinderCalls.map { it.methodName }.toList()
+        // Use `toSet` first so that we don't visit the same method multiple times.
+        parsedBinderCalls.map { it.methodName }.toSet().toList()
 
     override fun visitMethodCall(context: JavaContext, node: UCallExpression, method: PsiMethod) {
-        // TODO: b/469073407 - Handle the same method name in a different class/package gracefully.
-        val matchingBinderCall: BinderCall =
-            parsedBinderCalls.find { it.methodName == method.name } ?: return
+        val methodName = method.name
+        val packageName = context.evaluator.getPackage(method)?.qualifiedName
+        val className = (method.parent as? PsiClass)?.name ?: return
 
-        val className = method.parent
-        val classMatches = className is PsiClass && className.name == matchingBinderCall.className
-        val packageMatches =
-            context.evaluator.getPackage(method)?.qualifiedName == matchingBinderCall.packageName
-
-        if (!classMatches || !packageMatches) {
+        val isBinderCall: Boolean =
+            parsedBinderCalls.any {
+                methodName == it.methodName &&
+                    className == it.className &&
+                    packageName == it.packageName
+            }
+        if (!isBinderCall) {
             return
         }
 
