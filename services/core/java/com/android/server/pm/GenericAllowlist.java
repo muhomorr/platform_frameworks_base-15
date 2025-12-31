5fe7b31a55dad1255f98f929720443ef9496fbfb
Felipe Leme <felipeal@google.com>
2025-12-30 16:53:08 -0800

    Changed NotificationManagerService to use new method to log HSU notifications.
    Test: echo "it compiles"
    Bug: 412176703
    Bug: 414326600
    Flag: android.multiuser.hsu_allowlist_notifications
    
    Change-Id: I15625b5671b3ef23f3de621fd98aaa8562f84904

--- services/core/java/com/android/server/pm/GenericAllowlist.java
+++ services/core/java/com/android/server/pm/GenericAllowlist.java
@@ -36,6 +36,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+// TODO(b/412176703): currently public because NMS uses a constant and the
+// UserNotificationsAllowlist class doesn't exist yet - once it does, make it package protected.
 /**
  * Base class responsible for managing generic allowlists.
  *
@@ -50,7 +52,7 @@ import java.util.stream.Stream;
  *
  * @param <E> type of the element being allowlisted.
  */
-abstract class GenericAllowlist<E> {
+public abstract class GenericAllowlist<E> {
 
     @VisibleForTesting
     static final boolean DEBUG = Log.isLoggable(GenericAllowlist.class.getSimpleName(), Log.DEBUG);
