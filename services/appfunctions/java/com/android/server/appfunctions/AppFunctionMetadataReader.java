184ba7a0595d4eb3f6d29405e81ce9808d3f43bc
MingWei Liao <mingweiliao@google.com>
2026-01-06 09:24:16 +0000

    Handle dynamic AppFunction enabled state for searchAppFunctions
    Move the logic of checking enabled state into AppFunctionMetadataReader
    to ensure that all functions can share the same logic to decide the
    enabled state of an AppFunction.
    
    Flag: android.app.appfunctions.flags.enable_dynamic_app_functions
    Bug: 472644908
    Test: atest
    Change-Id: I43f2dfb409455cf36bfd54ac1bd525dfc8451fcf

--- services/appfunctions/java/com/android/server/appfunctions/AppFunctionMetadataReader.java
+++ services/appfunctions/java/com/android/server/appfunctions/AppFunctionMetadataReader.java
@@ -15,8 +15,11 @@
  */
 package com.android.server.appfunctions;
 
+import static android.app.appfunctions.AppFunctionManager.APP_FUNCTION_STATE_DEFAULT;
+import static android.app.appfunctions.AppFunctionManager.APP_FUNCTION_STATE_ENABLED;
 import static android.app.appfunctions.AppFunctionRuntimeMetadata.APP_FUNCTION_RUNTIME_NAMESPACE;
 import static android.app.appfunctions.AppFunctionRuntimeMetadata.PROPERTY_APP_FUNCTION_STATIC_METADATA_QUALIFIED_ID;
+import static android.app.appfunctions.AppFunctionRuntimeMetadata.PROPERTY_ENABLED;
 import static android.app.appfunctions.AppFunctionStaticMetadataHelper.APP_FUNCTION_INDEXER_PACKAGE;
 import static android.app.appfunctions.AppFunctionStaticMetadataHelper.APP_FUNCTION_STATIC_NAMESPACE;
 
@@ -26,18 +29,21 @@ import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.annotation.PermissionManuallyEnforced;
 import android.annotation.WorkerThread;
+import android.app.appfunctions.AppFunctionManagerHelper;
 import android.app.appfunctions.AppFunctionMetadata;
+import android.app.appfunctions.AppFunctionName;
 import android.app.appfunctions.AppFunctionPackageMetadata;
 import android.app.appfunctions.AppFunctionRuntimeMetadata;
 import android.app.appfunctions.AppFunctionSearchSpec;
+import android.app.appfunctions.AppFunctionStaticMetadataHelper;
 import android.app.appfunctions.IAppFunctionSearchResultCallback;
 import android.app.appfunctions.IAppFunctionSearchResults;
 import android.app.appsearch.GenericDocument;
 import android.app.appsearch.JoinSpec;
+import android.app.appsearch.PropertyPath;
 import android.app.appsearch.SearchResult;
 import android.app.appsearch.SearchSpec;
 import android.app.appsearch.observer.DocumentChangeInfo;
-import android.content.Context;
 import android.os.Binder;
 import android.os.ParcelableException;
 import android.os.RemoteException;
@@ -58,7 +64,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
 /** Manages app functions search requests. */
-public class AppFunctionMetadataReader {
+final class AppFunctionMetadataReader {
     private static final String TAG = AppFunctionMetadataReader.class.getSimpleName();
 
     private static final SearchSpec RUNTIME_SEARCH_SPEC =
@@ -71,13 +77,17 @@ public class AppFunctionMetadataReader {
             new JoinSpec.Builder(PROPERTY_APP_FUNCTION_STATIC_METADATA_QUALIFIED_ID)
                     .setNestedSearch("", RUNTIME_SEARCH_SPEC)
                     .build();
-
+    private final MultiUserDynamicAppFunctionRegistry mMultiUserDynamicAppFunctionRegistry;
     private final AppFunctionsMetadataCache mCache;
     private final ServiceConfig mServiceConfig;
 
     public AppFunctionMetadataReader(
-            @NonNull Context context, @NonNull ServiceConfig serviceConfig) {
-        mCache = new AppFunctionsMetadataCache(Objects.requireNonNull(context));
+            @NonNull MultiUserDynamicAppFunctionRegistry multiUserDynamicAppFunctionRegistry,
+            @NonNull AppFunctionsMetadataCache metadataCache,
+            @NonNull ServiceConfig serviceConfig) {
+        mMultiUserDynamicAppFunctionRegistry =
+                Objects.requireNonNull(multiUserDynamicAppFunctionRegistry);
+        mCache = Objects.requireNonNull(metadataCache);
         mServiceConfig = Objects.requireNonNull(serviceConfig);
     }
 
@@ -117,6 +127,62 @@ public class AppFunctionMetadataReader {
     }
 
     /**
+     * Checks if the {@code targetAppFunction} is enabled or not.
+     *
+     * @param futureGlobalSearchSession The session to search AppFunctions.
+     * @param targetAppFunction The target AppFunction.
+     * @param userId The target user id.
+     * @return True if the function is enabled. False otherwise.
+     */
+    CompletableFuture<Boolean> isAppFunctionEnabled(
+            @NonNull FutureGlobalSearchSession futureGlobalSearchSession,
+            @NonNull AppFunctionName targetAppFunction,
+            int userId) {
+        SearchSpec appFunctionJoinedStaticWithRuntimeSearchSpec =
+                new SearchSpec.Builder()
+                        .addFilterDocumentIds(List.of(targetAppFunction.getQualifiedId()))
+                        .addFilterPackageNames(APP_FUNCTION_INDEXER_PACKAGE)
+                        .addFilterSchemas(
+                                AppFunctionStaticMetadataHelper.getStaticSchemaNameForPackage(
+                                        targetAppFunction.getPackageName()))
+                        .addProjectionPaths(
+                                SearchSpec.SCHEMA_TYPE_WILDCARD,
+                                List.of(
+                                        new PropertyPath(
+                                                AppFunctionMetadata.PROPERTY_ENABLED_BY_DEFAULT)))
+                        .setJoinSpec(JOIN_SPEC)
+                        .setVerbatimSearchEnabled(true)
+                        .build();
+        return futureGlobalSearchSession
+                .search("", appFunctionJoinedStaticWithRuntimeSearchSpec)
+                .thenCompose(FutureSearchResults::getNextPage)
+                .thenCompose(
+                        result -> {
+                            if (result.size() != 1) {
+                                return AndroidFuture.failedFuture(
+                                        new AppFunctionManagerHelper.AppFunctionNotFoundException(
+                                                "App function not found."));
+                            }
+                            List<SearchResult> joinedResult = result.getFirst().getJoinedResults();
+                            if (joinedResult.size() != 1) {
+                                return AndroidFuture.failedFuture(
+                                        new RuntimeException(
+                                                TextUtils.formatSimple(
+                                                        "Expected 1 GenericDocument for"
+                                                                + " runtimeMetadata, found %d",
+                                                        joinedResult.size())));
+                            }
+
+                            GenericDocument staticDocument = result.getFirst().getGenericDocument();
+                            GenericDocument runtimeDocument =
+                                    joinedResult.getFirst().getGenericDocument();
+                            return AndroidFuture.completedFuture(
+                                    calculateEffectiveEnabledState(
+                                            staticDocument, runtimeDocument, userId));
+                        });
+    }
+
+    /**
      * Performs a one-time search for AppFunctionMetadata with the given searchSpec and returns the
      * result.
      *
@@ -126,13 +192,13 @@ public class AppFunctionMetadataReader {
      * @param resultExecutor The executor to be used by {@link IAppFunctionSearchResults} when
      *     reading the next page.
      */
-    // TODO(b/438413081): Handle enabled state of contextual app functions.
     @WorkerThread
     @NonNull
     IAppFunctionSearchResults searchAppFunctions(
             @NonNull FutureGlobalSearchSession futureGlobalSearchSession,
             @NonNull AppFunctionSearchSpec appFunctionSearchSpec,
-            @NonNull Executor resultExecutor)
+            @NonNull Executor resultExecutor,
+            int userId)
             throws ExecutionException, InterruptedException {
         requireNonNull(futureGlobalSearchSession);
         requireNonNull(appFunctionSearchSpec);
@@ -158,7 +224,7 @@ public class AppFunctionMetadataReader {
                                 appFunctionJoinedStaticWithRuntimeSearchSpec)
                         .get();
         return new AppFunctionSearchResultsImpl(
-                futureGlobalSearchSession, futureSearchResults, resultExecutor);
+                this, futureGlobalSearchSession, futureSearchResults, resultExecutor, userId);
     }
 
     /**
@@ -168,36 +234,71 @@ public class AppFunctionMetadataReader {
     @VisibleForTesting
     @Nullable
     @WorkerThread
-    public static AppFunctionMetadata convertSearchResultToAppFunctionMetadata(
-            @NonNull SearchResult searchResult) {
+    public AppFunctionMetadata buildAppFunctionMetadata(
+            @NonNull SearchResult searchResult, int userId) {
         try {
-            GenericDocument staticMetadataDocument =
-                    requireNonNull(searchResult.getGenericDocument());
+            GenericDocument staticDocument = requireNonNull(searchResult.getGenericDocument());
             if (requireNonNull(searchResult.getJoinedResults()).size() != 1) {
                 throw new IllegalArgumentException(
                         TextUtils.formatSimple(
                                 "Expected 1 GenericDocument for runtimeMetadata, found %d",
                                 searchResult.getJoinedResults().size()));
             }
-            GenericDocument runtimeMetadataDocument =
+            GenericDocument runtimeDocument =
                     requireNonNull(searchResult.getJoinedResults().getFirst().getGenericDocument());
             String packageName =
                     requireNonNull(
-                            runtimeMetadataDocument.getPropertyString(
+                            runtimeDocument.getPropertyString(
                                     AppFunctionRuntimeMetadata.PROPERTY_PACKAGE_NAME));
 
-            return AppFunctionMetadata.create(
-                    staticMetadataDocument,
-                    runtimeMetadataDocument,
-                    // TODO(b/438413081): Add package-level components
-                    AppFunctionPackageMetadata.create(packageName, Collections.emptyList()));
+            return new AppFunctionMetadata.Builder(
+                            staticDocument,
+                            // TODO(b/438413081): Add package-level components
+                            AppFunctionPackageMetadata.create(packageName, Collections.emptyList()))
+                    .setEnabled(
+                            calculateEffectiveEnabledState(staticDocument, runtimeDocument, userId))
+                    .build();
         } catch (RuntimeException e) {
             Slog.e(TAG, "Failed to convert SearchResult to AppFunctionMetadata.", e);
             return null;
         }
     }
 
+    private boolean calculateEffectiveEnabledState(
+            @NonNull GenericDocument staticDocument,
+            @NonNull GenericDocument runtimeDocument,
+            int userId) {
+        Objects.requireNonNull(staticDocument);
+        Objects.requireNonNull(runtimeDocument);
+
+        boolean isRuntimeEnabled;
+        if (runtimeDocument.getPropertyLong(PROPERTY_ENABLED) == APP_FUNCTION_STATE_DEFAULT) {
+            isRuntimeEnabled =
+                    staticDocument.getPropertyBoolean(
+                            AppFunctionMetadata.PROPERTY_ENABLED_BY_DEFAULT);
+        } else {
+            isRuntimeEnabled =
+                    runtimeDocument.getPropertyLong(PROPERTY_ENABLED) == APP_FUNCTION_STATE_ENABLED;
+        }
+
+        AppFunctionName appFunctionName = AppFunctionName.fromQualifiedId(staticDocument.getId());
+        String packageName = appFunctionName.getPackageName();
+        String functionId = appFunctionName.getFunctionId();
+
+        if (mCache.isDynamicFunction(packageName, functionId, UserHandle.of(userId))) {
+            return isRuntimeEnabled
+                    && mMultiUserDynamicAppFunctionRegistry.isAppFunctionRegistered(
+                            packageName, functionId, UserHandle.of(userId));
+        } else {
+            // TODO(b/467317154): Take into account AppFunctionService availability for static
+            //  app functions
+            return isRuntimeEnabled;
+        }
+    }
+
     private static class AppFunctionSearchResultsImpl extends IAppFunctionSearchResults.Stub {
+        @NonNull private final AppFunctionMetadataReader mReader;
+
         @GuardedBy("mLock")
         @NonNull
         private final FutureGlobalSearchSession mGlobalSession;
@@ -208,18 +309,24 @@ public class AppFunctionMetadataReader {
 
         @NonNull private final Executor mExecutor;
 
+        private final int mUserId;
+
         private final Object mLock = new Object();
 
         @GuardedBy("mLock")
         private Boolean mIsClosed = false;
 
         AppFunctionSearchResultsImpl(
+                @NonNull AppFunctionMetadataReader reader,
                 @NonNull FutureGlobalSearchSession globalSession,
                 @NonNull FutureSearchResults searchResults,
-                @NonNull Executor executor) {
+                @NonNull Executor executor,
+                int userId) {
+            mReader = Objects.requireNonNull(reader);
             mGlobalSession = Objects.requireNonNull(globalSession);
             mSearchResults = Objects.requireNonNull(searchResults);
             mExecutor = Objects.requireNonNull(executor);
+            mUserId = userId;
         }
 
         @PermissionManuallyEnforced
@@ -276,9 +383,7 @@ public class AppFunctionMetadataReader {
                                             new ArrayList<>(page.size());
                                     for (SearchResult result : page) {
                                         AppFunctionMetadata meta =
-                                                AppFunctionMetadataReader
-                                                        .convertSearchResultToAppFunctionMetadata(
-                                                                result);
+                                                mReader.buildAppFunctionMetadata(result, mUserId);
                                         if (meta != null) metadataList.add(meta);
                                     }
 
