c555d1623a6a11a677ac42e92ea553cb040ef672
Jorge Gil <jorgegil@google.com>
2026-01-06 00:57:19 +0000

    [FS Req] Add veil to desktop->fullscreen animation
    Flag: com.android.window.flags.delegate_request_fullscreen_handling_to_shell
    Fix: 473484601
    Fix: 343725172
    Test: move from desktop fullscreen using drag, menu or API - verify
    animation is veiled
    
    Change-Id: I304f2edd9ac65d12f4f3e7863dff5492b7ba7083

--- libs/WindowManager/Shell/src/com/android/wm/shell/desktopmode/DesktopTasksController.kt
+++ libs/WindowManager/Shell/src/com/android/wm/shell/desktopmode/DesktopTasksController.kt
@@ -440,6 +440,7 @@ class DesktopTasksController(
     fun setOnTaskResizeAnimationListener(listener: OnTaskResizeAnimationListener) {
         toggleResizeDesktopTaskTransitionHandler.setOnTaskResizeAnimationListener(listener)
         enterDesktopTaskTransitionHandler.setOnTaskResizeAnimationListener(listener)
+        exitDesktopTaskTransitionHandler.setOnTaskResizeAnimationListener(listener)
         dragToDesktopTransitionHandler.onTaskResizeAnimationListener = listener
         desktopImmersiveController.onTaskResizeAnimationListener = listener
         desktopFullscreenRequestHandler.onTaskResizeAnimationListener = listener
