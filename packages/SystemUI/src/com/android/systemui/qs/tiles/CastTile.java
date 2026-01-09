c73bdf0e4b5deabe9335ca73e1ab7854cc941702
Luca Zuccarini <acul@google.com>
2026-01-09 11:42:13 +0000

    Clean up aconfig flag fix_cast_dialog_flicker.
    Fix: 474383869
    Flag: EXEMPT FLAG_REMOVAL
    Test: EXEMPT FLAG_REMOVAL
    Change-Id: I550eeb5c2598b6f5f252b932931667ddb74fb95d

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CastTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CastTile.java
@@ -36,7 +36,6 @@ import com.android.internal.app.MediaRouteDialogPresenter;
 import com.android.internal.jank.InteractionJankMonitor;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.systemui.Flags;
 import com.android.systemui.animation.ActivityTransitionAnimator;
 import com.android.systemui.animation.DialogCuj;
 import com.android.systemui.animation.DialogTransitionAnimator;
@@ -249,9 +248,7 @@ public class CastTile extends QSTileImpl<BooleanState> {
             SystemUIDialog.registerDismissListener(dialog);
             SystemUIDialog.setWindowOnTop(dialog, mKeyguard.isShowing());
             SystemUIDialog.setDialogSize(dialog);
-            if (Flags.fixCastDialogFlicker()) {
-                SystemUIDialog.resetElevation(dialog);
-            }
+            SystemUIDialog.resetElevation(dialog);
 
             mUiHandler.post(() -> {
                 if (expandable != null) {
