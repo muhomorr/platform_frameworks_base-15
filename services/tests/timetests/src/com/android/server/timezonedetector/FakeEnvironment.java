82c2bc57851e85cf4c8deaf67a969439cdb605a9
ELIYAZ MOMIN (xWF) <mohammedeliyaz@google.com>
2026-01-15 04:22:12 -0800

    Revert^2 "Add logging for time zone transition and disagrement."
    This reverts commit bac0347de41637272a48ad9280432431debe786a.
    
    Reason for revert: <reverting back, not a culprit for b/476024060 but merged based on ABTD result>
    
    Bug: 417458133
    Change-Id: I4381f981c4812dd1f44d611482c1380d022c6353

--- services/tests/timetests/src/com/android/server/timezonedetector/FakeEnvironment.java
+++ services/tests/timetests/src/com/android/server/timezonedetector/FakeEnvironment.java
@@ -20,6 +20,7 @@ import static com.android.server.SystemTimeZone.TIME_ZONE_CONFIDENCE_LOW;
 
 import android.annotation.CurrentTimeMillisLong;
 import android.annotation.ElapsedRealtimeLong;
+import android.util.Pair;
 
 import com.android.server.SystemTimeZone;
 
@@ -35,6 +36,7 @@ public class FakeEnvironment implements Environment {
     private final TestState<String> mTimeZoneId = new TestState<>();
     private final TestState<Integer> mTimeZoneConfidence = new TestState<>();
     private final List<Runnable> mAsyncRunnables = new ArrayList<>();
+    private final List<Pair<Long, Runnable>> mDelayedRunnables = new ArrayList<>();
     private @ElapsedRealtimeLong long mElapsedRealtimeMillis;
     private @CurrentTimeMillisLong long mInitializationTimeMillis;
 
@@ -57,6 +59,10 @@ public class FakeEnvironment implements Environment {
 
     void incrementClock() {
         mElapsedRealtimeMillis++;
+    }
+
+    void advanceClock(@ElapsedRealtimeLong long durationMillis) {
+        mElapsedRealtimeMillis += durationMillis;
     }
 
     @Override
@@ -137,5 +143,43 @@ public class FakeEnvironment implements Environment {
             runnable.run();
         }
         mAsyncRunnables.clear();
+    }
+
+    @Override
+    public void postDelayed(Runnable runnable, long delayMillis) {
+        removePendingRunnable(runnable);
+        mDelayedRunnables.add(new Pair<>(delayMillis + elapsedRealtimeMillis(), runnable));
+    }
+
+    /**
+     * Checks if there are any delayed runnables that  have been supplied to {@code #postDelayed}
+     * and runs them if time is up.
+     */
+    public void runDelayedRunnables() {
+        ArrayList<Pair<Long, Runnable>> runnablesToRun = new ArrayList<>(mDelayedRunnables);
+        mDelayedRunnables.clear();
+        for (Pair<Long, Runnable> pair : runnablesToRun) {
+            if (pair.first > elapsedRealtimeMillis()) {
+                mDelayedRunnables.add(new Pair<>(pair.first, pair.second));
+                continue;
+            }
+            pair.second.run();
+        }
+    }
+
+    @Override
+    public void removePendingRunnable(Runnable runnable) {
+        ArrayList<Pair<Long, Runnable>> runnablesToRun = new ArrayList<>(mDelayedRunnables);
+        mDelayedRunnables.clear();
+        for (Pair<Long, Runnable> pair : runnablesToRun) {
+            if (pair.second == runnable) {
+                continue;
+            }
+            mDelayedRunnables.add(new Pair<>(pair.first, pair.second));
+        }
+    }
+
+    public int getDelayedRunnableCount() {
+        return mDelayedRunnables.size();
     }
 }
