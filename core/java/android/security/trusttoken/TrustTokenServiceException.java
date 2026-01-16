291ccd0199583987eb001b4eb020cbab725d9c7d
Ross Wu <rosswu@google.com>
2026-01-16 01:56:02 +0000

    Use OutcomeReceiver instead of TrustTokenCallback.
    Flag: android.security.talisman_service_api
    Bug: 459301404,476105824
    Test: CTS: I02400010499048073
    Change-Id: I620cb7124bfc3323b47219662ab90541958c8ac1

--- core/java/android/security/trusttoken/TrustTokenServiceException.java
+++ core/java/android/security/trusttoken/TrustTokenServiceException.java
@@ -26,14 +26,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Callback invoked by the {@link TrustTokenService} when received trust tokens from the remote
- * Trust Anchor server.
+ * Represents an error encountered during a trust token operation.
  *
  * @hide
  */
 @FlaggedApi(Flags.FLAG_TALISMAN_SERVICE_API)
 @SystemApi
-public abstract class TrustTokenCallback {
+public final class TrustTokenServiceException extends Exception {
     /**
      * Error codes returned by the remote Trust Anchor server.
      *
@@ -57,22 +56,23 @@ public abstract class TrustTokenCallback {
     /** The request failed due to an unavailable error (such as network unavailable). */
     public static final int ERROR_UNAVAILABLE = 3;
 
-    /**
-     * Invoked when successfully received trust tokens and other data from the remote server.
-     *
-     * @param response the response from the remote server.
-     */
-    public abstract void onSuccess(@NonNull TrustTokenResponse response);
+    @ErrorCode
+    private final int mErrorCode;
 
     /**
-     * Invoked when encountered a permanent error, or too many retry attempts for transient errors
-     * (such as network unavailable).
+     * Constructs a {@link TrustTokenServiceException}.
      *
-     * <p>For permanent errors, the system should stop retrying. For transient errors, the service
-     * has internally retried for certain times but still failed, so the system should wait for a
-     * while then retry.
-     *
-     * @param code the error code returned by the remote server.
+     * @param errorCode The error code.
+     * @param message The error message.
      */
-    public abstract void onFailure(@ErrorCode int code);
+    public TrustTokenServiceException(@ErrorCode int errorCode, @NonNull String message) {
+        super(message);
+        mErrorCode = errorCode;
+    }
+
+    /** Returns the error code. */
+    @ErrorCode
+    public int getErrorCode() {
+        return mErrorCode;
+    }
 }
