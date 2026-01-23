a4988f9f357d98444b6bb1a1bbd028f0c446d3a9
Navin G V <navingv@google.com>
2026-01-23 05:29:24 -0800

    Added TestNonPccJobService, TestNonPccService, TestPccJobService with PCC flag for testing the execution of different PCC service launch flow.
    Added buttons to Main activity to trigger different flows: 1. Schedule a
    constrained Pcc-Job from Pcc Service, 2. Schedule a Pcc Job Service from
    Non-Pcc Main Activity - No known use cases for this scenario so far. 3.
    Start Non-Pcc Service from PccService, 4. Start Non-Pcc Service from Pcc
    Job Service,
    
    Test: Manually tested on a device.
      1. Build and install PccTestApp.
      2. Launch the app.
      3. Click different buttons to validate each scenarios.
    
    Scenarios & Outcome:
    
    1. Schedule a constrained Pcc-Job from Pcc Service: Working as expected
    
    2. Schedule a Pcc Job Service from Non-Pcc Main Activity: Failed as
    expected. Sample Exception: java.lang.IllegalArgumentException: uid
    10328 cannot schedule job in com.example.pcctestapp
    
    3. Start Non-Pcc Service from PccService: Failed as expected. Sample
    Exception: java.lang.SecurityException: Not allowed to start service
    Intent { xflg=0x4 cmp=com.example.pcctestapp/.TestNonPccService (has
    extras) } without permission association not allowed between packages
    com.example.pcctestapp (uid=30328) and com.example.pcctestapp
    (uid=10328)
    
    4. Start Non-Pcc Service from Pcc Job Service: Failed as expected.
    Sample Exception: java.lang.RuntimeException:
    java.lang.SecurityException: Not allowed to start service Intent {
    xflg=0x4 cmp=com.example.pcctestapp/.TestNonPccService (has extras) }
    without permission association not allowed between packages
    com.example.pcctestapp (uid=30328) and com.example.pcctestapp
    (uid=10328)
    
    Bug: b/472969404
    Flag: EXEMPT TEST_ONLY
    Change-Id: I88c5f35d0feea2fd64a8421f3918105d29e6d8fb

--- tests/privatecompute/manual-test-apps/PccTestApp/src/com/example/pcctestapp/MainActivity.java
+++ tests/privatecompute/manual-test-apps/PccTestApp/src/com/example/pcctestapp/MainActivity.java
@@ -23,12 +23,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.Gravity;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -39,14 +41,15 @@ public class MainActivity extends Activity {
     private final ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName className, IBinder service) {
-            logAndShowToast(Log.INFO, "Service connected!");
+            DisplayUtils.logAndShowToast(MainActivity.this, Log.INFO, TAG, "Service connected!");
             mClient = PccClient.createInstance(MainActivity.this, service);
             mIsPccServiceBound = true;
         }
 
         @Override
         public void onServiceDisconnected(ComponentName className) {
-            logAndShowToast(Log.INFO, "Service disconnected unexpectedly.");
+            DisplayUtils.logAndShowToast(
+                    MainActivity.this, Log.INFO, TAG, "Service disconnected unexpectedly.");
             mClient = null;
             mIsPccServiceBound = false;
         }
@@ -72,7 +75,7 @@ public class MainActivity extends Activity {
                         "com.example.pcctestapp.TestPccService"));
                 bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
             } else {
-                logAndShowToast(Log.WARN, "Service is already bound.");
+                DisplayUtils.logAndShowToast(this, Log.WARN, TAG, "Service is already bound.");
             }
         });
 
@@ -82,18 +85,106 @@ public class MainActivity extends Activity {
         sendDataButton.setOnClickListener(v -> {
             if (mIsPccServiceBound) {
                 Bundle data = new Bundle();
-                data.putString("my_key", "Hello World");
+                data.putString(Constants.KEY_MY_KEY, "Hello World");
                 try {
                     mClient.sendData(data);
-                    logAndShowToast(Log.INFO, "Data sent to Pcc service.");
+                    DisplayUtils.logAndShowToast(this, Log.INFO, TAG, "Data sent to Pcc service.");
                 } catch (Exception e) {
-                    logAndShowToast(Log.WARN, "Failed to send data to Pcc service. " + e);
+                    DisplayUtils.logAndShowToast(
+                            this, Log.WARN, TAG, "Failed to send data to Pcc service. " + e);
                 }
             } else {
-                logAndShowToast(Log.ERROR,
+                DisplayUtils.logAndShowToast(this, Log.ERROR, TAG,
                         "Pcc service is not bound yet. Bind to Pcc service first.");
             }
         });
+
+
+        // Button to schedule a constrained Pcc job from the pcc service.
+        Button scheduleConstrainedPccJobFromPccServiceButton = new Button(this);
+        scheduleConstrainedPccJobFromPccServiceButton.setText(
+            "Schedule a constrained Pcc-Job from Pcc service");
+        scheduleConstrainedPccJobFromPccServiceButton.setOnClickListener(v -> {
+            if (mIsPccServiceBound) {
+                Bundle data = new Bundle();
+                data.putBoolean(Constants.KEY_SCHEDULE_CONSTRAINED_JOB, true);
+                try {
+                    mClient.sendData(data);
+                    DisplayUtils.logAndShowToast(this, Log.INFO, TAG,
+                            "Request to schedule a constrained job sent to Pcc service.");
+                } catch (Exception e) {
+                    DisplayUtils.logAndShowToast(this, Log.WARN, TAG,
+                            "Failed to send request to schedule a constrained job. " + e);
+                }
+            } else {
+                DisplayUtils.logAndShowToast(this, Log.ERROR, TAG,
+                        "Pcc service is not bound yet. Bind to Pcc service first.");
+            }
+        });
+
+        // Button to schedule a constrained job from the main activity using TestJobService.
+        Button scheduleConstrainedPccJobFromNonPccMainActivityButton = new Button(this);
+        scheduleConstrainedPccJobFromNonPccMainActivityButton.setText(
+            "Schedule a constrained Pcc-Job from Non Pcc Main Activity (will fail)");
+        scheduleConstrainedPccJobFromNonPccMainActivityButton.setOnClickListener(
+                v -> JobServiceScheduler.scheduleConstrainedTestPccJob(this));
+
+        // Button to send data from the pcc service to non pcc service.
+        Button sendDataFromPccServiceToNonPccServiceButton = new Button(this);
+        sendDataFromPccServiceToNonPccServiceButton.setText(
+            "Send data from Pcc service to Non Pcc service (not allowed)");
+        sendDataFromPccServiceToNonPccServiceButton.setOnClickListener(v -> {
+            if (mIsPccServiceBound) {
+                Bundle data = new Bundle();
+                data.putBoolean(Constants.KEY_SEND_DATA_TO_NON_PCC, true);
+                try {
+                    mClient.sendData(data);
+                    DisplayUtils.logAndShowToast(this, Log.INFO, TAG,
+                            "Request to send data to Non Pcc service sent to Pcc service.");
+                } catch (Exception e) {
+                    DisplayUtils.logAndShowToast(this, Log.WARN, TAG,
+                            "Failed to send request to send data to Non Pcc service. " + e);
+                }
+            } else {
+                DisplayUtils.logAndShowToast(this, Log.ERROR, TAG,
+                        "Pcc service is not bound yet. Bind to Pcc service first.");
+            }
+        });
+
+        // Button to schedule an unconstrained job from the pcc service to start a non-pcc service.
+        Button scheduleUnconstrainedPccJobToStartNonPccServiceButton = new Button(this);
+        scheduleUnconstrainedPccJobToStartNonPccServiceButton.setText(
+                "Schedule Unconstrained PCC-Job to start Non-PCC-Service (not allowed)");
+        scheduleUnconstrainedPccJobToStartNonPccServiceButton.setOnClickListener(
+                v -> {
+                    if (mIsPccServiceBound) {
+                        Bundle data = new Bundle();
+                        data.putBoolean(
+                                Constants.KEY_SCHEDULE_UNCONSTRAINED_JOB_AND_START_NON_PCC_SERVICE,
+                                true);
+                        try {
+                            mClient.sendData(data);
+                            DisplayUtils.logAndShowToast(
+                                    this,
+                                    Log.INFO,
+                                    TAG,
+                                    "Request to schedule a job and start non-pcc service sent to"
+                                            + " Pcc service.");
+                        } catch (Exception e) {
+                            DisplayUtils.logAndShowToast(
+                                    this,
+                                    Log.WARN,
+                                    TAG,
+                                    "Failed to send request to schedule a job. " + e);
+                        }
+                    } else {
+                        DisplayUtils.logAndShowToast(
+                                this,
+                                Log.ERROR,
+                                TAG,
+                                "Pcc service is not bound yet. Bind to Pcc service first.");
+                    }
+                });
 
 
         // Button to unbind from the pcc service.
@@ -103,26 +194,36 @@ public class MainActivity extends Activity {
             if (mIsPccServiceBound) {
                 Log.i(TAG, "Unbinding service...");
                 unbindService(mConnection);
-                logAndShowToast(Log.INFO, "Service disconnected.");
+                DisplayUtils.logAndShowToast(this, Log.INFO, TAG, "Service disconnected.");
                 mIsPccServiceBound = false;
             } else {
-                logAndShowToast(Log.ERROR, "Service is not bound, cannot unbind.");
+                DisplayUtils.logAndShowToast(
+                        this, Log.ERROR, TAG, "Service is not bound, cannot unbind.");
             }
         });
 
-
         layout.addView(bindButton);
         layout.addView(sendDataButton);
+
+        layout.addView(scheduleConstrainedPccJobFromPccServiceButton);
+
+        layout.addView(scheduleConstrainedPccJobFromNonPccMainActivityButton);
+
+        layout.addView(sendDataFromPccServiceToNonPccServiceButton);
+        layout.addView(scheduleUnconstrainedPccJobToStartNonPccServiceButton);
+
         layout.addView(unbindButton);
+
         setContentView(layout);
     }
 
-    private void logAndShowToast(int logLevel, String message) {
-        switch (logLevel) {
-            case Log.INFO -> Log.i(TAG, message);
-            case Log.WARN -> Log.w(TAG, message);
-            case Log.ERROR -> Log.e(TAG, message);
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mIsPccServiceBound) {
+            Log.i(TAG, "Unbinding service on activity destroy to prevent service leaks.");
+            unbindService(mConnection);
+            mIsPccServiceBound = false;
         }
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
