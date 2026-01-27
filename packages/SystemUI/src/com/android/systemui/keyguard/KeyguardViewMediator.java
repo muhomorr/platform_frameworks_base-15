3c66ea199aed07e3996ae8f8492e455c15e6a8c0
Alexander Hendrich <hendrich@google.com>
2026-01-27 10:39:26 -0800

    Revert "Dismiss keyguard earlier during user switch"
    This reverts commit be7cbd2da55a976f623f5f5e12a742cc3f92acf8.
    
    Reason for revert: https://b.corp.google.com/issues/478911993#comment5 (waiting for keyguard dismissal stalls handleUserSwitching(), which stalls UserTracker)
    
    Fix: 478911993
    Change-Id: Ifaf31c5f45c517553f8743c2bc59333b0c33ee8c

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -721,33 +721,10 @@ public class KeyguardViewMediator implements CoreStartable,
     void handleUserSwitching(int userId, Runnable resultCallback) {
         Log.d(TAG, String.format("onUserSwitching %d", userId));
         synchronized (KeyguardViewMediator.this) {
-            if (!SceneContainerFlag.isEnabled()) {
-                if (!mLockPatternUtils.isSecure(userId)) {
-                    dismiss(null, null);
-                }
-                resultCallback.run();
-            } else {
-                // Calling dismiss on a secure user will show the bouncer.
-                // We wait for the dismiss to complete here as otherwise the user switching dialog
-                // might fade out while keyguard is still dismissing.
-                dismiss(new IKeyguardDismissCallback.Stub() {
-                            @Override
-                            public void onDismissError() throws RemoteException {
-                                resultCallback.run();
-                            }
-
-                            @Override
-                            public void onDismissSucceeded() throws RemoteException {
-                                resultCallback.run();
-                            }
-
-                            @Override
-                            public void onDismissCancelled() throws RemoteException {
-                                resultCallback.run();
-                            }
-                        },
-                        null);
+            if (!mLockPatternUtils.isSecure(userId)) {
+                dismiss(null, null);
             }
+            resultCallback.run();
         }
     }
 
@@ -757,14 +734,12 @@ public class KeyguardViewMediator implements CoreStartable,
     @VisibleForTesting
     void handleUserSwitchComplete(int userId) {
         Log.d(TAG, String.format("onUserSwitchComplete %d", userId));
-        if (!SceneContainerFlag.isEnabled()) {
-            // Calling dismiss on a secure user will show the bouncer
-            if (mLockPatternUtils.isSecure(userId)) {
-                // We are calling dismiss with a delay as there are race conditions in some
-                // scenarios caused by async layout listeners
-                mHandler.postDelayed(
-                        () -> dismiss(null /* callback */, null /* message */), mDismissToken, 500);
-            }
+        // Calling dismiss on a secure user will show the bouncer
+        if (mLockPatternUtils.isSecure(userId)) {
+            // We are calling dismiss with a delay as there are race conditions in some scenarios
+            // caused by async layout listeners
+            mHandler.postDelayed(() -> dismiss(null /* callback */, null /* message */),
+                    mDismissToken, 500);
         }
         // We need to adjust the status bar in case there are race conditions where the
         // previous adjust event was sent before the user switch completed.
