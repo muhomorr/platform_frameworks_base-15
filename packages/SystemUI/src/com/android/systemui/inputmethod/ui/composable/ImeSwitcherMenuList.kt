05fb450359f2226d6f860d1b2a3836d89b9c4382
Michelle Chen <michellegc@google.com>
2026-01-28 13:22:40 -0800

    Extract ImeSwitcherMenuList into separate file and add test.
    This is to make it easier to share the `ImeSwitcherMenuList` between the
    current IME Switcher Menu layout and the new large screen layout we will
    be adding.
    
    Bug: 475339762
    Flag: EXEMPT PURE_REFACTOR
    Test: atest SystemUITests:ImeSwitcherMenuListTest
    Change-Id: If97a01f0e3a9524b25c461e9818e9e3efd13f59d

---
+++ packages/SystemUI/src/com/android/systemui/inputmethod/ui/composable/ImeSwitcherMenuList.kt
@@ -0,0 +1,188 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.inputmethod.ui.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.selected
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
+import com.android.internal.R
+import com.android.systemui.inputmethod.ui.viewmodel.ImeSwitcherMenuViewModel
+
+/**
+ * The UI for the list of IMEs and IME subtypes.
+ *
+ * @param items the UI items for IMEs and IME subtypes.
+ * @param viewModel the view model to get the data from.
+ * @param dismissAction the action to invoke when the UI should be dismissed.
+ */
+@Composable
+fun ImeSwitcherMenuList(
+    items: List<ImeSwitcherMenuViewModel.MenuItem>,
+    viewModel: ImeSwitcherMenuViewModel,
+    dismissAction: () -> Unit,
+) {
+    val listState = rememberLazyListState()
+    // Start the UI scrolled to the selected item, and react to changes in the selected item,
+    // scrolling to the new position.
+    LaunchedEffect(viewModel.selectedIndex.intValue, items.size) {
+        val index = viewModel.selectedIndex.intValue
+        if (index != -1 && index < items.size) {
+            listState.scrollToItem(index)
+        }
+    }
+
+    // TODO(b/308488505): Add scroll indicators to the LazyColumn once implemented.
+    LazyColumn(
+        state = listState,
+        contentPadding = PaddingValues(top = 8.dp),
+        modifier = Modifier.heightIn(max = 373.dp),
+    ) {
+        itemsIndexed(items, key = { _, item -> "${item.imeId}:${item.subtypeIndex}" }) { index, item
+            ->
+            ImeSwitcherMenuListItem(
+                item,
+                index == viewModel.selectedIndex.intValue,
+                viewModel,
+                dismissAction,
+            )
+        }
+    }
+}
+
+/**
+ * The UI for a single IME or IME subtype list item.
+ *
+ * @param item the IME or IME subtype to display.
+ * @param isSelected whether this is the currently selected item in the list.
+ * @param viewModel the view model to get the data from.
+ * @param dismissAction the action to invoke when the UI should be dismissed.
+ */
+@Composable
+private fun ImeSwitcherMenuListItem(
+    item: ImeSwitcherMenuViewModel.MenuItem,
+    isSelected: Boolean,
+    viewModel: ImeSwitcherMenuViewModel,
+    dismissAction: () -> Unit,
+) {
+    Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)) {
+        if (item.hasDivider) {
+            HorizontalDivider(
+                modifier =
+                    Modifier.fillMaxWidth()
+                        .padding(start = 20.dp, top = 8.dp, end = 24.dp, bottom = 16.dp),
+                color = MaterialTheme.colorScheme.outlineVariant,
+            )
+        }
+
+        if (item.hasHeader) {
+            Text(
+                text = item.imeName.toString(),
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.primary,
+                modifier =
+                    Modifier.fillMaxWidth()
+                        .padding(horizontal = 8.dp)
+                        .padding(top = 4.dp, bottom = 16.dp),
+            )
+        }
+
+        val backgroundColor =
+            if (isSelected) MaterialTheme.colorScheme.secondaryContainer else Color.Transparent
+        val selectedColor =
+            if (isSelected) MaterialTheme.colorScheme.onSecondaryContainer
+            else MaterialTheme.colorScheme.onSurface
+        Row(
+            modifier =
+                Modifier.fillMaxWidth()
+                    .padding(bottom = 8.dp)
+                    .heightIn(min = 72.dp)
+                    .clip(RoundedCornerShape(28.dp))
+                    .background(backgroundColor)
+                    .semantics { this.selected = isSelected }
+                    .clickable(
+                        onClick = {
+                            if (!isSelected) {
+                                viewModel.onImeAndSubtypeSelected(item.imeId, item.subtypeIndex)
+                            }
+                            dismissAction.invoke()
+                        }
+                    )
+                    .padding(start = 20.dp, end = 24.dp)
+                    .padding(vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                val text = if (item.subtypeName.isNullOrEmpty()) item.imeName else item.subtypeName
+                Text(
+                    text = text.toString(),
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = selectedColor,
+                    maxLines = 1,
+                    modifier = if (isSelected) Modifier.basicMarquee(iterations = 1) else Modifier,
+                )
+                if (!item.layoutName.isNullOrEmpty()) {
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = item.layoutName.toString().uppercase(),
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        maxLines = 1,
+                        modifier =
+                            if (isSelected) Modifier.basicMarquee(iterations = 1) else Modifier,
+                    )
+                }
+            }
+
+            if (isSelected) {
+                Icon(
+                    painter = painterResource(com.android.systemui.res.R.drawable.ic_check),
+                    contentDescription = null, // decorative
+                    tint = selectedColor,
+                    modifier = Modifier.padding(start = 12.dp).size(24.dp),
+                )
+            }
+        }
+    }
+}
