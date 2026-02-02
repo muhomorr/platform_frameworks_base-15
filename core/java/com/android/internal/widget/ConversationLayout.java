cac948e0e28c0e465ef4883bd567d318b597d812
Julia Reynolds <juliacr@google.com>
2026-02-02 09:43:13 -0500

    Allow summarizations to be 5 lines talls
    Test: manual
    Flag: EXEMPT BUGFIX
    Fixes: 452538825
    Change-Id: I1e6e1e4fdf39daa0f9394ae10a4159b7c4945947

--- core/java/com/android/internal/widget/ConversationLayout.java
+++ core/java/com/android/internal/widget/ConversationLayout.java
@@ -81,7 +81,7 @@ public class ConversationLayout extends FrameLayout
     public static final Interpolator FAST_OUT_SLOW_IN = new PathInterpolator(0.4f, 0f, 0.2f, 1f);
     public static final Interpolator OVERSHOOT = new PathInterpolator(0.4f, 0f, 0.2f, 1.4f);
     private static final String TAG = "ConversationLayout";
-    private static final int MAX_SUMMARIZATION_LINES = 3;
+    private static final int MAX_SUMMARIZATION_LINES = 5;
     public static final int IMPORTANCE_ANIM_GROW_DURATION = 250;
     public static final int IMPORTANCE_ANIM_SHRINK_DURATION = 200;
     public static final int IMPORTANCE_ANIM_SHRINK_DELAY = 25;
