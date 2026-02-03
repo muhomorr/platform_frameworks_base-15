674b268f4282834cd0d062008bb48438e2cbfc32
Pascal Mütschard <pmuetschard@google.com>
2026-02-03 16:19:13 +0100

    Don't use animation time to schedule callbacks on a Handler.
    Handler expects the timestamp to be in the SystemClock domain. There is
    no guarantee that animation time is in that same domain. This fixes the
    scheduling of the scrollbar fade-out to use the SystemClock domain, but
    then animate the fadeout using the animation domain.
    
    Note that currently the implementation of the animation time is based on
    the same time domain, but we are planning on changing that. Therefore,
    this change has no observable effect.
    
    Bug: b/470321586
    Test: presubmit
    Flag: EXEMPT BUGFIX
    Change-Id: I3e8d3de1af69cca24d171401f6881ee3cf04cff7

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -21337,7 +21337,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
 
             // Tell mScrollCache when we should start fading. This may
             // extend the fade start time if one was already scheduled
-            long fadeStartTime = AnimationUtils.currentAnimationTimeMillis() + startDelay;
+            long fadeStartTime = SystemClock.uptimeMillis() + startDelay;
             scrollCache.fadeStartTime = fadeStartTime;
             scrollCache.state = ScrollabilityCache.ON;
 
@@ -33366,7 +33366,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
         }
 
         public void run() {
-            long now = AnimationUtils.currentAnimationTimeMillis();
+            long now = SystemClock.uptimeMillis();
             if (now >= fadeStartTime) {
                 fadeScrollBarsScheduled = false;
                 handler = null;
@@ -33374,7 +33374,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
                 // the animation fades the scrollbars out by changing
                 // the opacity (alpha) from fully opaque to fully
                 // transparent
-                int nextFrame = (int) now;
+                int nextFrame = (int) AnimationUtils.currentAnimationTimeMillis();
                 int framesCount = 0;
 
                 Interpolator interpolator = scrollBarInterpolator;
