5687eed02e042ef7e14663be6e9976220450e222
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-02-10 12:05:16 -0800

    Revert "Implement IEncryptedStoreKEK in AISealSystemService"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/4d242551-a7e3-4387-8ffd-b123ab01610e). Please review this revert and approve if it looks good to you.
    
    Bug: 483292672
    Change-Id: I074fdd1c091b968583699a11a8fb1b0925464535
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 461527055

--- services/aiseal/java/com/android/server/aiseal/AiSealSystemService.java
+++ services/aiseal/java/com/android/server/aiseal/AiSealSystemService.java
@@ -17,11 +17,9 @@
 package com.android.server.aiseal;
 
 import android.content.Context;
-import android.os.Environment;
 import android.os.IBinder;
 import android.os.IBinder.DeathRecipient;
 import android.os.RemoteException;
-import android.os.SELinux;
 import android.os.ServiceManager;
 import android.text.format.DateUtils;
 import android.util.ArraySet;
@@ -33,15 +31,12 @@ import com.android.internal.os.BackgroundThread;
 import com.android.server.SystemService;
 import com.android.server.SystemService.TargetUser;
 
-import java.io.File;
 import java.util.Set;
 
 /** AiSeal system service. */
 public class AiSealSystemService extends SystemService {
 
     private static final String TAG = "AiSealSystemService";
-    private static final String AISEAL_PRIVATE_FOLDER = "AiSeal";
-    private static final String KEK_FILENAME = "kek";
 
     // Synchronizes user state changes with the AiSeal internal service connection.
     private static final Object sLock = new Object();
@@ -125,7 +120,11 @@ public class AiSealSystemService extends SystemService {
     @GuardedBy("sLock")
     private void onAiSealInternalServiceConnectedLocked() {
         for (int userId : mUnlockedUsers) {
-            notifyUserUnlockingLocked(userId);
+            try {
+                mAiSealInternalService.onUserUnlocking(userId);
+            } catch (Exception e) {
+                Slog.wtf(TAG, "Unable to unlock user " + userId, e);
+            }
         }
     }
 
@@ -154,11 +153,7 @@ public class AiSealSystemService extends SystemService {
     @GuardedBy("sLock")
     public void notifyUserUnlockingLocked(int userId) {
         try {
-            File kekFile = Environment.buildPath(Environment.getDataSystemCeDirectory(userId),
-                    AISEAL_PRIVATE_FOLDER, KEK_FILENAME);
-            kekFile.getParentFile().mkdirs();
-            SELinux.restorecon(kekFile.getParentFile());
-            mAiSealInternalService.onUserUnlocking(userId, kekFile.getAbsolutePath());
+            mAiSealInternalService.onUserUnlocking(userId);
         } catch (Exception e) {
             Slog.wtf(TAG, "Unable to unlock user " + userId, e);
         }
