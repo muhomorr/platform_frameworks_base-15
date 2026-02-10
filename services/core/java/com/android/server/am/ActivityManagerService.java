f6bd4cddb8700f6ac0d9be2c95b03bf2ecdc7443
Sudheer Shanka <sudheersai@google.com>
2026-02-10 15:12:33 -0800

    Augment PROCESS_START_TIME atom with provider info.
    The PROCESS_START_TIME atom currently lacks specific context
    when a process is launched due to a content provider. This
    change adds the provider's authority and whether if the provider
    is stable or unstable to the atom.
    
    Bug: 478260528
    Test: atest services/tests/mockingservicestests/src/com/android/server/am/HostingRecordTest.java
    Test: atest services/tests/mockingservicestests/src/com/android/server/am/ProcessStartTimeTest.java
    Flag: EXEMPT METRICS
    Change-Id: I1e487b46e1278a7d8d1182bb9fb5703fd2c4650a

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -5525,7 +5525,9 @@ public class ActivityManagerService extends IActivityManager.Stub
                     HostingRecord.getHostingTypeIdStatsd(hostingRecord.getType()),
                     HostingRecord.getTriggerTypeForStatsd(hostingRecord.getTriggerType()),
                     hostingRecord.getCallerUid(),
-                    hostingRecord.getCallerProcessName());
+                    hostingRecord.getCallerProcessName(),
+                    hostingRecord.getHostingAuthority(),
+                    hostingRecord.isProviderStable());
         }
     }
 
