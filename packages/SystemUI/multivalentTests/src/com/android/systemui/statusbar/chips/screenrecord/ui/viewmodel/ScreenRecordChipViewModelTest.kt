de934ad37d208774a1bbeb0e757fa7e76d7e7144
Gauri Shankar <gshnakar@google.com>
2026-02-10 09:57:36 +0000

    [2/n] Fixed and added tests to support new DialogTransitionAnimator#show API.
    Updating ag/38292529: I had old code, and there are some new changes in the ScreenRecordTileUserActionInteractorTest file, which was causing the break. Pulled, rebased and updated the fix.
    
    Bug: 412899675
    Flag: com.android.systemui.animation_library_dynamic_target_resolution
    Test: NONE
    Change-Id: I21ece0fd594127e19c03298be8bed01029d4ef6b

--- packages/SystemUI/multivalentTests/src/com/android/systemui/statusbar/chips/screenrecord/ui/viewmodel/ScreenRecordChipViewModelTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/statusbar/chips/screenrecord/ui/viewmodel/ScreenRecordChipViewModelTest.kt
@@ -374,6 +374,7 @@ class ScreenRecordChipViewModelTest : SysuiTestCase() {
         Flags.FLAG_NEW_SCREEN_RECORD_TOOLBAR,
         Flags.FLAG_LARGE_SCREEN_SCREENCAPTURE,
         Flags.FLAG_LARGE_SCREEN_RECORDING,
+        Flags.FLAG_ANIMATION_LIBRARY_DYNAMIC_TARGET_RESOLUTION,
     )
     fun chip_notProjecting_expandActionBehaviorShowsDialog() =
         testScope.runTest {
@@ -395,6 +396,29 @@ class ScreenRecordChipViewModelTest : SysuiTestCase() {
         Flags.FLAG_LARGE_SCREEN_SCREENCAPTURE,
         Flags.FLAG_LARGE_SCREEN_RECORDING,
     )
+    @EnableFlags(Flags.FLAG_ANIMATION_LIBRARY_DYNAMIC_TARGET_RESOLUTION)
+    fun chip_notProjecting_expandActionBehaviorShowsDialog_withDynamicTargetResolution() =
+        testScope.runTest {
+            val latest by collectLastValue(underTest.chip)
+            screenRecordRepo.screenRecordState.value = ScreenRecordModel.Recording
+            mediaProjectionRepo.mediaProjectionState.value = MediaProjectionState.NotProjecting
+
+            val expandAction =
+                ((latest as OngoingActivityChipModel.Active).clickBehavior
+                    as OngoingActivityChipModel.ClickBehavior.ExpandAction)
+
+            expandAction.onClick(mockExpandable)
+            verify(kosmos.mockDialogTransitionAnimator)
+                .show(any(), anyOrNull(), anyOrNull(), anyBoolean())
+        }
+
+    @Test
+    @DisableFlags(
+        Flags.FLAG_NEW_SCREEN_RECORD_TOOLBAR,
+        Flags.FLAG_LARGE_SCREEN_SCREENCAPTURE,
+        Flags.FLAG_LARGE_SCREEN_RECORDING,
+        Flags.FLAG_ANIMATION_LIBRARY_DYNAMIC_TARGET_RESOLUTION,
+    )
     fun chip_projectingEntireScreen_expandActionBehaviorShowsDialog() =
         testScope.runTest {
             val latest by collectLastValue(underTest.chip)
@@ -414,6 +438,28 @@ class ScreenRecordChipViewModelTest : SysuiTestCase() {
         Flags.FLAG_LARGE_SCREEN_SCREENCAPTURE,
         Flags.FLAG_LARGE_SCREEN_RECORDING,
     )
+    @EnableFlags(Flags.FLAG_ANIMATION_LIBRARY_DYNAMIC_TARGET_RESOLUTION)
+    fun chip_projectingEntireScreen_expandActionBehaviorShowsDialog_withDynamicTargetResolution() =
+        testScope.runTest {
+            val latest by collectLastValue(underTest.chip)
+            screenRecordRepo.screenRecordState.value = ScreenRecordModel.Recording
+
+            val expandAction =
+                ((latest as OngoingActivityChipModel.Active).clickBehavior
+                    as OngoingActivityChipModel.ClickBehavior.ExpandAction)
+
+            expandAction.onClick(mockExpandable)
+            verify(kosmos.mockDialogTransitionAnimator)
+                .show(any(), anyOrNull(), anyOrNull(), anyBoolean())
+        }
+
+    @Test
+    @DisableFlags(
+        Flags.FLAG_NEW_SCREEN_RECORD_TOOLBAR,
+        Flags.FLAG_LARGE_SCREEN_SCREENCAPTURE,
+        Flags.FLAG_LARGE_SCREEN_RECORDING,
+        Flags.FLAG_ANIMATION_LIBRARY_DYNAMIC_TARGET_RESOLUTION,
+    )
     fun chip_projectingSingleTask_expandActionBehaviorShowsDialog() =
         testScope.runTest {
             val latest by collectLastValue(underTest.chip)
@@ -431,6 +477,33 @@ class ScreenRecordChipViewModelTest : SysuiTestCase() {
 
             expandAction.onClick(mockExpandable)
             verify(kosmos.mockDialogTransitionAnimator).show(any(), any(), anyBoolean())
+        }
+
+    @Test
+    @DisableFlags(
+        Flags.FLAG_NEW_SCREEN_RECORD_TOOLBAR,
+        Flags.FLAG_LARGE_SCREEN_SCREENCAPTURE,
+        Flags.FLAG_LARGE_SCREEN_RECORDING,
+    )
+    @EnableFlags(Flags.FLAG_ANIMATION_LIBRARY_DYNAMIC_TARGET_RESOLUTION)
+    fun chip_projectingSingleTask_expandActionBehaviorShowsDialog_withDynamicTargetResolution() =
+        testScope.runTest {
+            val latest by collectLastValue(underTest.chip)
+            screenRecordRepo.screenRecordState.value = ScreenRecordModel.Recording
+            mediaProjectionRepo.mediaProjectionState.value =
+                MediaProjectionState.Projecting.SingleTask(
+                    "host.package",
+                    hostDeviceName = null,
+                    FakeActivityTaskManager.createTask(taskId = 1),
+                )
+
+            val expandAction =
+                ((latest as OngoingActivityChipModel.Active).clickBehavior
+                    as OngoingActivityChipModel.ClickBehavior.ExpandAction)
+
+            expandAction.onClick(mockExpandable)
+            verify(kosmos.mockDialogTransitionAnimator)
+                .show(any(), anyOrNull(), anyOrNull(), anyBoolean())
         }
 
     @Test
