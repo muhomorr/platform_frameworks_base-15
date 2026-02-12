651f5ccb01a475a3a1e51cfb313a5d03e17273d0
Graciela Putri <gracielawputri@google.com>
2026-02-12 09:59:55 -0800

    Fix build breakage from no flags import
    Flag: EXEMPT BUGFIX
    Bug: NA
    Test: m
    Change-Id: Ie39b10fc5e03abdb74c289774835978d5db16a45

--- libs/WindowManager/Shell/src/com/android/wm/shell/compatui/letterbox/LetterboxCommandHandler.kt
+++ libs/WindowManager/Shell/src/com/android/wm/shell/compatui/letterbox/LetterboxCommandHandler.kt
@@ -19,6 +19,7 @@ package com.android.wm.shell.compatui.letterbox
 import android.content.Context
 import android.graphics.Color
 import com.android.internal.protolog.ProtoLog
+import com.android.window.flags.Flags
 import com.android.wm.shell.dagger.WMSingleton
 import com.android.wm.shell.protolog.ShellProtoLogGroup.WM_SHELL_APP_COMPAT
 import com.android.wm.shell.sysui.ShellCommandHandler
