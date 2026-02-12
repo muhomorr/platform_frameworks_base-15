4a8b2881f51a4691028e5d174c1a5eb19adf56b3
Ale Nijamkin <nijamkin@google.com>
2026-02-12 10:24:18 -0800

    [flexiglass] Moves scene framework logging to queued background thread.
    This is an attempt to improve performance, particularly around the
    CPU/battery cost of logging on the main thread / compose critical path
    while keeping all of our debug-friendly logs available in any and all
    bugreports.
    
    Bug: 480353763
    Test: observed normal, in-order logging when expanding/collapsing the
    shade and locking/unlocking
    Flag: com.android.systemui.scene_container
    
    Change-Id: I9dd06d4c26c5d9ff1a66fd0ff99bc33390e0959a

--- packages/SystemUI/src/com/android/systemui/scene/domain/startable/SceneContainerStartable.kt
+++ packages/SystemUI/src/com/android/systemui/scene/domain/startable/SceneContainerStartable.kt
@@ -186,6 +186,7 @@ constructor(
 
     override fun start() {
         if (SceneContainerFlag.isEnabled) {
+            applicationScope.launch { sceneLogger.activate() }
             sceneLogger.logFrameworkEnabled(isEnabled = true)
             applicationScope.launch { hydrateTableLogBuffer() }
             maybeShowLockscreenOnStart()
