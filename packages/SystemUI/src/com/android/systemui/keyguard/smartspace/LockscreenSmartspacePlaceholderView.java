87e5f6a8a21e98d89fb9ed577fc5c45af4e544d0
liamsmith827 <liamsmith827@proton.me>
2026-02-15 04:02:27 +0000

    add LockscreenSmartspacePlaceholderView

---
+++ packages/SystemUI/src/com/android/systemui/keyguard/smartspace/LockscreenSmartspacePlaceholderView.java
@@ -0,0 +1,13 @@
+package com.android.systemui.keyguard.smartspace;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+import com.android.systemui.smartspace.ui.DefaultSmartspaceView;
+
+public class LockscreenSmartspacePlaceholderView extends LinearLayout
+        implements DefaultSmartspaceView {
+    public LockscreenSmartspacePlaceholderView(Context context) {
+        super(context);
+    }
+}
