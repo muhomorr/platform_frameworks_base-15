c56aa80ed237c91e02752f1e37cefe920c5cd9e8
Haifan Wang <wanghaifan@google.com>
2026-02-18 17:22:59 -0800

    Revert "Hide DND from Quick Settings in Desktop Retail Demo Mode"
    This reverts commit 6845e84cb69f636d88e92c99f95f67c7d6984f9d.
    
    Reason for revert: ModesTile will be deprecated soon. Replaced with an easier approach https://arsp-review.git.corp.google.com/c/platform/vendor/google/desktop/+/29593
    
    Change-Id: I524136e62c8d52ca47c3426c1332162645ed91f7

--- packages/SystemUI/multivalentTests/src/com/android/systemui/qs/tiles/ModesTileTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/qs/tiles/ModesTileTest.kt
@@ -59,7 +59,6 @@ import com.android.systemui.qs.tiles.impl.modes.domain.model.ModesTileModel
 import com.android.systemui.qs.tiles.impl.modes.domain.model.ModesTileModel.ActiveMode
 import com.android.systemui.qs.tiles.impl.modes.ui.mapper.ModesTileMapper
 import com.android.systemui.res.R
-import com.android.systemui.retail.domain.interactor.RetailModeInteractor
 import com.android.systemui.shade.domain.interactor.shadeInteractor
 import com.android.systemui.statusbar.policy.data.repository.zenModeRepository
 import com.android.systemui.statusbar.policy.domain.interactor.zenModeInteractor
@@ -110,8 +109,6 @@ class ModesTileTest(flags: FlagsParameterization) : SysuiTestCase() {
     @Mock private lateinit var qsTileConfigProvider: QSTileConfigProvider
 
     @Mock private lateinit var dialogDelegate: ModesDialogDelegate
-
-    @Mock private lateinit var retailModeInteractor: RetailModeInteractor
 
     init {
         mSetFlagsRule.setFlagsParameterization(flags)
@@ -190,7 +187,6 @@ class ModesTileTest(flags: FlagsParameterization) : SysuiTestCase() {
                 mapper,
                 userActionInteractor,
                 kosmos.modesDialogViewModel,
-                retailModeInteractor,
             )
 
         underTest.initialize()
@@ -203,36 +199,6 @@ class ModesTileTest(flags: FlagsParameterization) : SysuiTestCase() {
     fun tearDown() {
         underTest.destroy()
         testableLooper.processAllMessages()
-    }
-
-    @Test
-    fun isAvailable_hideDNDinQSinRetailDemoMode_returnsFalse() {
-        whenever(retailModeInteractor.isInRetailMode).thenReturn(true)
-        context
-            .getOrCreateTestableResources()
-            .addOverride(R.bool.config_hideDNDinQSinRetailDemoMode, true)
-
-        assertThat(underTest.isAvailable).isFalse()
-    }
-
-    @Test
-    fun isAvailable_notInRetailDemoMode_returnsTrue() {
-        whenever(retailModeInteractor.isInRetailMode).thenReturn(false)
-        context
-            .getOrCreateTestableResources()
-            .addOverride(R.bool.config_hideDNDinQSinRetailDemoMode, true)
-
-        assertThat(underTest.isAvailable).isTrue()
-    }
-
-    @Test
-    fun isAvailable_notHideDNDinQSinRetailDemoMode_returnsTrue() {
-        whenever(retailModeInteractor.isInRetailMode).thenReturn(true)
-        context
-            .getOrCreateTestableResources()
-            .addOverride(R.bool.config_hideDNDinQSinRetailDemoMode, false)
-
-        assertThat(underTest.isAvailable).isTrue()
     }
 
     @Test
