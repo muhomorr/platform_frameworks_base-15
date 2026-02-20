85def9fcc93f9754d32a3bc54888584c77022498
Alexander Hendrich <hendrich@google.com>
2026-02-20 00:30:48 -0800

    Fix UserSwitcheer Icon coloring
    Current code applied a tint, which rendered all the Icons in a single color. We want to preserve the original image's colors though.
    
    Fix: 459984007
    Flag: com.android.systemui.scene_container
    Test: manual verification
    Change-Id: Ie72c574b64588a2428b02c78a569536bb9eba279

--- packages/SystemUI/compose/features/src/com/android/systemui/bouncer/ui/composable/BouncerContent.kt
+++ packages/SystemUI/compose/features/src/com/android/systemui/bouncer/ui/composable/BouncerContent.kt
@@ -77,6 +77,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -1134,7 +1135,7 @@ private fun UserSwitcherDropdownMenu(
                     leadingIcon = {
                         Icon(
                             icon = userSwitcherDropdownItem.icon,
-                            tint = MaterialTheme.colorScheme.primary,
+                            tint = Color.Unspecified, // Preserve the icon's original colors.
                             modifier = Modifier.size(28.dp),
                         )
                     },
