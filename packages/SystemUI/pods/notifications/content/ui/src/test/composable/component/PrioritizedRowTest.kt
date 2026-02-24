5981c558135a7e714b23197935ccbd181a1109ee
Ioana Alexandru <aioana@google.com>
2026-02-24 18:04:13 +0100

    Move PrioritizedRowTest to its pod
    Pods didn't yet have a test setup defined when we created this class, so
    we added the test to SystemUI directly. Now we can actually move it to
    the correct directory.
    
    Temporarily ignoring the test with the full width, since it's failing
    with this test configuration. See b/439930983.
    
    Bug: 431222735
    Test: atest PrioritizedRowTest
    Flag: EXEMPT PURE_REFACTOR
    Change-Id: I3cd09aa1aeca42b1c933693ae5210396af1197b9

--- packages/SystemUI/pods/notifications/content/ui/src/test/composable/component/PrioritizedRowTest.kt
+++ packages/SystemUI/pods/notifications/content/ui/src/test/composable/component/PrioritizedRowTest.kt
@@ -47,8 +47,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.android.compose.theme.PlatformTheme
 import com.android.systemui.SysuiTestCase
-import kotlin.test.Test
+import org.junit.Ignore
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @SmallTest
@@ -69,6 +70,7 @@ class PrioritizedRowTest : SysuiTestCase() {
     private val reducedWidth = 50.dp
     private val hideWidth = 20.dp
 
+    @Ignore("b/439930983")
     @Test
     fun widthFull_allChildrenAreVisible() {
         rule.setContent {
