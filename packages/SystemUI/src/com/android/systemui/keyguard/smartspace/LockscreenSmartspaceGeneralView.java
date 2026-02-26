092e436f5d464ae4e40d412949c586de37014201
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-02-26 12:36:11 +0000

    improve LockscreenSmartspaceGeneralView
    - reuse LockscreenSmartspaceGeneralTextViews to improve performance
    - rename confusingly named drawLine() method. Drawing is separate from layout management.
    - rename confusingly named Row class
    - don't cache the value of R.attr.wallpaperTextColor

--- packages/SystemUI/src/com/android/systemui/keyguard/smartspace/LockscreenSmartspaceGeneralView.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/smartspace/LockscreenSmartspaceGeneralView.java
@@ -6,14 +6,12 @@ import android.app.smartspace.uitemplatedata.BaseTemplateData;
 import android.app.smartspace.uitemplatedata.CombinedCardsTemplateData;
 import android.app.smartspace.uitemplatedata.Icon;
 import android.app.smartspace.uitemplatedata.Text;
-import android.content.ComponentName;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.InsetDrawable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -25,65 +23,74 @@ import com.android.systemui.smartspace.ui.DefaultSmartspaceView;
 
 import java.util.List;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * Smartspace view for displaying date, zen mode, upcoming alarms and media. This is a
  * reimplementation of the legacy KeyguardSliceView.
  */
 public class LockscreenSmartspaceGeneralView extends LinearLayout implements DefaultSmartspaceView {
-
-    private static final int DARK_AMOUNT = 0;
-
-    private LockscreenSmartspaceGeneralView.Row mRow;
-    private int mTextColor;
-    private int mIconSize;
-
+    private Rows mRows;
+    private final int mIconSize;
 
     public LockscreenSmartspaceGeneralView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mIconSize = (int) mContext.getResources().getDimension(R.dimen.widget_icon_size);
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        mRow = findViewById(R.id.row);
-        mTextColor = Utils.getColorAttrDefaultColor(mContext, R.attr.wallpaperTextColor);
-        mIconSize = (int) mContext.getResources().getDimension(R.dimen.widget_icon_size);
+        mRows = findViewById(R.id.rows);
     }
 
     protected void showTarget(SmartspaceTarget target) {
-        // Remove old views.
-        // TODO: Improve efficiency by reusing existing views and only removing those which are no
-        // longer needed.
-        for (int i = 0; i < mRow.getChildCount(); i++) {
-            View child = mRow.getChildAt(i);
-            mRow.removeView(child);
-            i--;
-        }
-
-        CombinedCardsTemplateData template = (CombinedCardsTemplateData)target.getTemplateData();
+        var template = (CombinedCardsTemplateData) requireNonNull(target.getTemplateData());
         List<BaseTemplateData> subTemplates = template.getCombinedCardDataList();
 
-        mRow.setVisibility(!subTemplates.isEmpty() ? VISIBLE : GONE);
-        LinearLayout.LayoutParams layoutParams = (LayoutParams) mRow.getLayoutParams();
-        layoutParams.gravity = Gravity.START;
-        mRow.setLayoutParams(layoutParams);
+        if (subTemplates.isEmpty()) {
+            mRows.setVisibility(View.GONE);
+            return;
+        }
+
+        mRows.setVisibility(View.VISIBLE);
+
+        int viewIdx = 0;
+
+        final int textColor = ColorUtils.blendARGB(
+                Utils.getColorAttrDefaultColor(mContext, R.attr.wallpaperTextColor),
+                Color.WHITE, 0.0f);
 
         for (BaseTemplateData data : subTemplates) {
             BaseTemplateData.SubItemInfo primaryItem = data.getPrimaryItem();
-            BaseTemplateData.SubItemInfo subTitleItem = data.getSubtitleItem();
             if (primaryItem != null) {
-                drawLine(primaryItem);
+                addRow(primaryItem, textColor, viewIdx++);
+                BaseTemplateData.SubItemInfo subTitleItem = data.getSubtitleItem();
                 if (subTitleItem != null) {
-                    drawLine(subTitleItem);
+                    addRow(subTitleItem, textColor, viewIdx++);
                 }
             }
         }
+
+        int rowCount = mRows.getChildCount();
+        if (viewIdx != rowCount) {
+            if (viewIdx > rowCount) {
+                throw new IllegalStateException();
+            }
+            mRows.removeViews(viewIdx, rowCount - viewIdx);
+        }
     }
 
-    private void drawLine(BaseTemplateData.SubItemInfo subItemInfo) {
-        LockscreenSmartspaceGeneralTextView tv = new LockscreenSmartspaceGeneralTextView(mContext);
-        tv.setTextColor(getTextColor());
-        mRow.addView(tv, -1);
+    private void addRow(BaseTemplateData.SubItemInfo subItemInfo, int textColor, int viewIdx) {
+        LockscreenSmartspaceGeneralTextView tv;
+        boolean isCachedView = viewIdx < mRows.getChildCount();
+        if (isCachedView) {
+            tv = (LockscreenSmartspaceGeneralTextView) mRows.getChildAt(viewIdx);
+        } else {
+            tv = new LockscreenSmartspaceGeneralTextView(mContext);
+        }
+        tv.setTextColor(textColor);
 
         Text text = subItemInfo.getText();
         tv.setText(text == null ? null : text.getText());
@@ -93,10 +100,10 @@ public class LockscreenSmartspaceGeneralView extends LinearLayout implements Def
         if (icon != null) {
             iconDrawable = icon.getIcon().loadDrawable(mContext);
             if (iconDrawable != null) {
-                if (iconDrawable instanceof InsetDrawable) {
+                if (iconDrawable instanceof InsetDrawable insetDrawable) {
                     // System icons (DnD) use insets which are fine for centered slice content
                     // but will cause a slight indent for left/right-aligned slice views.
-                    iconDrawable = ((InsetDrawable) iconDrawable).getDrawable();
+                    iconDrawable = requireNonNull(insetDrawable.getDrawable());
                 }
                 final int width = (int) (iconDrawable.getIntrinsicWidth()
                         / (float) iconDrawable.getIntrinsicHeight() * mIconSize);
@@ -104,56 +111,51 @@ public class LockscreenSmartspaceGeneralView extends LinearLayout implements Def
             }
         }
         tv.setCompoundDrawablesRelative(iconDrawable, null, null, null);
-    }
 
-    private int getTextColor() {
-        return ColorUtils.blendARGB(mTextColor, Color.WHITE, DARK_AMOUNT);
+        if (!isCachedView) {
+            mRows.addView(tv);
+        }
     }
 
     void onDensityOrFontScaleChanged() {
-        for (int i = 0; i < mRow.getChildCount(); i++) {
-            View child = mRow.getChildAt(i);
-            if (child instanceof LockscreenSmartspaceGeneralTextView) {
-                ((LockscreenSmartspaceGeneralTextView) child).onDensityOrFontScaleChanged();
+        for (int i = 0; i < mRows.getChildCount(); i++) {
+            View child = mRows.getChildAt(i);
+            if (child instanceof LockscreenSmartspaceGeneralTextView text) {
+                text.onDensityOrFontScaleChanged();
             }
         }
     }
 
     void onOverlayChanged() {
-        for (int i = 0; i < mRow.getChildCount(); i++) {
-            View child = mRow.getChildAt(i);
-            if (child instanceof LockscreenSmartspaceGeneralTextView) {
-                ((LockscreenSmartspaceGeneralTextView) child).onOverlayChanged();
+        for (int i = 0; i < mRows.getChildCount(); i++) {
+            View child = mRows.getChildAt(i);
+            if (child instanceof LockscreenSmartspaceGeneralTextView text) {
+                text.onOverlayChanged();
             }
         }
     }
 
-    public static class Row extends LinearLayout {
+    /** based on {@link com.android.keyguard.KeyguardSliceView.Row} */
+    public static class Rows extends LinearLayout {
 
-        public Row(Context context) {
-            this(context, null);
-        }
-
-        public Row(Context context, AttributeSet attrs) {
+        public Rows(Context context, AttributeSet attrs) {
             this(context, attrs, 0);
         }
 
-        public Row(Context context, AttributeSet attrs, int defStyleAttr) {
+        public Rows(Context context, AttributeSet attrs, int defStyleAttr) {
             this(context, attrs, defStyleAttr, 0);
         }
 
-        public Row(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        public Rows(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
             super(context, attrs, defStyleAttr, defStyleRes);
         }
 
         @Override
         protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-            int childCount = getChildCount();
-
-            for (int i = 0; i < childCount; i++) {
+            for (int i = 0; i < getChildCount(); i++) {
                 View child = getChildAt(i);
-                if (child instanceof LockscreenSmartspaceGeneralTextView) {
-                    ((LockscreenSmartspaceGeneralTextView) child).setMaxWidth(Integer.MAX_VALUE);
+                if (child instanceof LockscreenSmartspaceGeneralTextView text) {
+                    text.setMaxWidth(Integer.MAX_VALUE);
                 }
             }
 
@@ -166,7 +168,8 @@ public class LockscreenSmartspaceGeneralView extends LinearLayout implements Def
         }
     }
 
-    public static class LockscreenSmartspaceGeneralTextView extends TextView {
+    /** copied from {@link com.android.keyguard.KeyguardSliceView.KeyguardSliceTextView} */
+    static class LockscreenSmartspaceGeneralTextView extends TextView {
 
         @StyleRes
         private static final int sStyleId = R.style.TextAppearance_Keyguard_Secondary;
