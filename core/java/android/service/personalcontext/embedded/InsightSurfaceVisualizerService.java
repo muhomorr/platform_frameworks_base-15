0dd7709941575a71c8c691b2318be9e808740cb6
William Xiao <wxyz@google.com>
2026-03-02 23:41:23 +0000

    Require PERSONAL_CONTEXT_RECEIVE_INSIGHTS permission for InsightSurfaceVisualizerService
    createVisualizationForClient provides an insight, so this permission
    should be checked.
    
    Bug: 488530273
    Bug: 489183723
    Fixed: 488530273
    Test: atest VisualizerConnectionTest
    Flag: android.service.personalcontext.enforce_personal_context_permissions
    Change-Id: I89689d6e7785c06ca032cf123cae5839cc198538

--- core/java/android/service/personalcontext/embedded/InsightSurfaceVisualizerService.java
+++ core/java/android/service/personalcontext/embedded/InsightSurfaceVisualizerService.java
@@ -36,6 +36,8 @@ import android.os.ResultReceiver;
 import android.service.personalcontext.Flags;
 import android.service.personalcontext.IOpCallback;
 import android.service.personalcontext.RenderToken;
+import android.service.personalcontext.hint.ContextHint;
+import android.service.personalcontext.insight.ContextInsight;
 import android.service.personalcontext.insight.PublishedContextInsight;
 import android.service.personalcontext.insight.PublishedContextInsightWrapper;
 import android.service.personalcontext.util.BinderRequestProcessor;
@@ -84,15 +86,19 @@ import java.util.concurrent.Executor;
  * information about the properties that a client can report to a visualizer.
  *
  * <p>You must declare the service in the AndroidManifest of the app hosting the service with the
- * {@link Manifest.permission#BIND_INSIGHT_SURFACE_VISUALIZER_SERVICE} permission,
- * and include an intent filter with the necessary action indicating that it is an
- * {@link InsightSurfaceVisualizerService} ({@link #SERVICE_INTERFACE}).
+ * {@link Manifest.permission#BIND_INSIGHT_SURFACE_VISUALIZER_SERVICE} permission, and include an
+ * intent filter with the necessary action indicating that it is an {@link
+ * InsightSurfaceVisualizerService} ({@link #SERVICE_INTERFACE}). The application must have the
+ * {@link Manifest.permission#PERSONAL_CONTEXT_RECEIVE_INSIGHTS} permission.
  *
  * <p>For example:
  * <pre>
+ *     &lt;uses-permission
+ *         android:name="android.permission.PERSONAL_CONTEXT_RECEIVE_INSIGHTS"/&gt;
+ *
  *     &lt;service android:name=".ExampleInsightSurfaceVisualizerService"
- *             android:exported="true"
- *             android:permission="android.permission.BIND_INSIGHT_SURFACE_VISUALIZER_SERVICE"&gt;
+ *         android:exported="true"
+ *         android:permission="android.permission.BIND_INSIGHT_SURFACE_VISUALIZER_SERVICE"&gt;
  *         &lt;intent-filter&gt;
  *             &lt;action
  *           android:name="android.service.personalcontext.embedded.InsightSurfaceVisualizerService"
