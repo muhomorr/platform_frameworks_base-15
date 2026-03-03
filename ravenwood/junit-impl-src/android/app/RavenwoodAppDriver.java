5604019845dfb686b018daaa470680471531ddf1
John Wu <topjohnwu@google.com>
2026-03-03 23:41:50 +0000

    [Ravenwood] Support delegating methods for AIDL interface proxies
    Bug: 292141694
    Flag: EXEMPT TEST_ONLY
    Test: atest RavenwoodCoreTest
    Change-Id: Ie0100607428cb5d569ce3913d2d0b7eb69d29ae7

--- ravenwood/junit-impl-src/android/app/RavenwoodAppDriver.java
+++ ravenwood/junit-impl-src/android/app/RavenwoodAppDriver.java
@@ -188,7 +188,7 @@ public final class RavenwoodAppDriver {
         var mockSettings = RavenwoodProxyHelper.newProxy(
                 IContentProvider.class,
                 IContentProvider.descriptor,
-                mSettingsProvider, false);
+                mSettingsProvider);
         synchronized (mProviders) {
             mProviders.put(Settings.AUTHORITY, mockSettings);
         }
