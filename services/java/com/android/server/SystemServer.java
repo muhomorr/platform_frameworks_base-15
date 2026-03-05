224ba82c619fa42e98aaf7cbb6a0b3c998c84709
brycelee <brycelee@google.com>
2026-03-05 20:34:43 +0000

    Enable PersonalContextManagerService based on config.
    This changelist defines a config bool resource that determines
    whether PersonalContextManagerService should be enabled. This
    resource is false by default and enabled in resource overlays
    by product.
    
    Bug: 489877135
    Bug: 434644900
    Test: manual - ensured built
    Flag: android.service.personalcontext.enable_personal_context_service
    Change-Id: I507872df7b3705aac30d27efaeca8fd0fbb729da

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2671,7 +2671,8 @@ public final class SystemServer implements Dumpable {
                     new GraphicsStatsService(context));
             t.traceEnd();
 
-            if (android.service.personalcontext.Flags.enablePersonalContextService()) {
+            if (context.getResources().getBoolean(
+                    R.bool.config_enablePersonalContextManagerService)) {
                 t.traceBegin("StartPersonalContextService");
                 mSystemServiceManager.startService(PersonalContextManagerService.class);
                 t.traceEnd();
