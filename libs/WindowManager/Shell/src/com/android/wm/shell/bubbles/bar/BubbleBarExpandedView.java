c26acb67d4f532652d97cb11b0da5fa45703b435
Mady Mellor <madym@google.com>
2026-03-11 15:53:24 -0700

    Fix an issue where user education wasn't dismissed
    When the user taps explicitly on the handle while user education
    is showing, that triggers the menu to open (desired behavior). However,
    the user education isn't dismissed.
    
    This adds an event to let BubbleBarLayerView know that the handle
    menu is opened so that it can dismiss the user education if it
    is visible.
    
    Flag: com.android.wm.shell.fix_bubbles_handle_user_education
    Bug: 490187640
    Test: manual - force user education to show, tap on the handle menu
                 => observe that the handle menu opens and user education
                    is dismissed
                 - verify that tapping outside of the menu collapses the
                   menu
    Change-Id: I06bae659a1ab226b5c3002249699e5bc67e79520

--- libs/WindowManager/Shell/src/com/android/wm/shell/bubbles/bar/BubbleBarExpandedView.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/bubbles/bar/BubbleBarExpandedView.java
@@ -79,6 +79,8 @@ public class BubbleBarExpandedView extends FrameLayout implements BubbleTaskView
         void onUnBubbleConversation(String bubbleKey);
         /** Called when expanded view task view back button pressed */
         void onBackPressed();
+        /** Called when the handle menu is opened */
+        void onHandleMenuOpened();
     }
 
     /**
@@ -263,6 +265,9 @@ public class BubbleBarExpandedView extends FrameLayout implements BubbleTaskView
             public void onMenuVisibilityChanged(boolean visible) {
                 setObscured(visible);
                 if (visible) {
+                    if (mListener != null) {
+                        mListener.onHandleMenuOpened();
+                    }
                     getHandleView().setFocusable(false);
                     getHandleView().setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_NO);
                 } else {
@@ -561,7 +566,7 @@ public class BubbleBarExpandedView extends FrameLayout implements BubbleTaskView
         mLayerBoundsSupplier = supplier;
     }
 
-    /** Sets expanded view listener */
+    /** Sets listener used to notify {@link BubbleBarLayerView} of events  */
     void setListener(@Nullable Listener listener) {
         mListener = listener;
     }
