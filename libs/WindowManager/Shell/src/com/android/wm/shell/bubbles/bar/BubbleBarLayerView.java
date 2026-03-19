56df21080e7c4166251cb4a5ded709e6fff24fab
mpodolian <mpodolian@google.com>
2026-03-19 23:26:22 +0000

    Implement transition animation cancellation for bubbles
    Introduce animation cancellation support for BubbleStackView and
    BubbleLayerView. This ensures animations can be stopped mid-transit
    and return the current animated property values correctly.
    
    Bug: 483107404
    Test: BubbleTransitionsTest
    Flag: com.android.wm.shell.enable_bubble_transition_planner
    
    Change-Id: Ifab18138bd5b92ae4996e1a1bb5e3b2a20fbf504

--- libs/WindowManager/Shell/src/com/android/wm/shell/bubbles/bar/BubbleBarLayerView.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/bubbles/bar/BubbleBarLayerView.java
@@ -39,6 +39,7 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.window.WindowAnimationState;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
@@ -831,5 +832,11 @@ public class BubbleBarLayerView extends FrameLayout
         if (ContextUtils.isGestureNavigationMode(mContext)) {
             mBubbleBarGestureNavSwipeController.stopMonitoring();
         }
+    }
+
+    @Override
+    @Nullable
+    public WindowAnimationState cancelAnimation() {
+        return mAnimationHelper.cancelAnimationsAndReturnState();
     }
 }
