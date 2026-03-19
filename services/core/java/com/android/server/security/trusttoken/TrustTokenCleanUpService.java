50a12fc99cf3f3d2c238f8dd51b2b9c70417752c
Chaitanya Cheemala (xWF) <ccheemala@google.com>
2026-03-19 01:47:06 -0700

    Revert "Schedule jobs after user unlock"
    This reverts commit 6878ec02f8fd7a7f6005edfda41233de2abdd2fb.
    
    Reason for revert: Likely culprit for b/494124322 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Bug: 494124322
    Bug: 418280383
    Change-Id: I57d0ea03a20e5c7d99f141670efedef725cac2e2

--- services/core/java/com/android/server/security/trusttoken/TrustTokenCleanUpService.java
+++ services/core/java/com/android/server/security/trusttoken/TrustTokenCleanUpService.java
@@ -103,9 +103,9 @@ public class TrustTokenCleanUpService extends JobService {
                             .setRequiresCharging(true)
                             .setRequiresDeviceIdle(true);
             if (mJobScheduler.schedule(jobInfo.build()) == JobScheduler.RESULT_SUCCESS) {
-                Slog.i(TAG, "Regular trust token cleanup scheduled.");
+                Slog.i(TAG, "Regular trust token refresh scheduled.");
             } else {
-                Slog.e(TAG, "Regular trust token cleanup failed to schedule.");
+                Slog.e(TAG, "Regular trust token refresh failed to schedule.");
             }
         }
     }
