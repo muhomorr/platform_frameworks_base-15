c38f2ca23836366879f47677876bc80ca4a675fc
Priyanka Advani (xWF) <padvani@google.com>
2026-03-24 14:23:01 -0700

    Revert "Introduce LooperDoctor"
    This reverts commit 4b0f0be1229165a508414f36f7c26404dc015692.
    
    Reason for revert: Droidmonitor created revert due to b/495887996. Will be verifying through ABTD before submission.
    
    Fix: 495887996
    Bug: 483709211
    Change-Id: If176463b7a676ed4f3d98f057107f8a7b5c7dc6f

--- core/java/android/os/Looper.java
+++ core/java/android/os/Looper.java
@@ -36,8 +36,6 @@ import android.util.Printer;
 import android.util.Slog;
 import android.util.proto.ProtoOutputStream;
 
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.VarHandle;
 import java.util.Objects;
 
 /**
@@ -290,11 +288,6 @@ public final class Looper {
             token = observer.messageDispatchStarting();
         }
         long origWorkSource = ThreadLocalWorkSource.setUid(msg.workSourceUid);
-
-        final LooperDoctor doctor = Flags.messageQueueMonitoringEnabled() ? me.mLooperDoctor : null;
-        if (doctor != null) {
-            doctor.startMessageTimer(me.mAlarm);
-        }
         try {
             msg.target.dispatchMessage(msg);
             if (isLooperClearsThreadInterruptedEnabled()) {
@@ -312,9 +305,6 @@ public final class Looper {
             }
             throw exception;
         } finally {
-            if (doctor != null) {
-                doctor.stopMessageTimer(me.mAlarm);
-            }
             ThreadLocalWorkSource.restore(origWorkSource);
             if (traceTag != 0) {
                 Trace.traceEnd(traceTag);
@@ -566,7 +556,6 @@ public final class Looper {
      * @see #quitSafely
      */
     public void quit() {
-        clearLooperDoctor();
         mQueue.quit(false);
     }
 
@@ -586,7 +575,6 @@ public final class Looper {
      * </p>
      */
     public void quitSafely() {
-        clearLooperDoctor();
         mQueue.quit(true);
     }
 
@@ -606,48 +594,6 @@ public final class Looper {
      */
     public @NonNull MessageQueue getQueue() {
         return mQueue;
-    }
-
-    private static final VarHandle sLooperDoctor;
-    private volatile LooperDoctor mLooperDoctor;
-    private LooperDoctor.LooperDoctorAlarm mAlarm;
-
-    static {
-        try {
-            MethodHandles.Lookup l = MethodHandles.lookup();
-            sLooperDoctor = l.findVarHandle(Looper.class, "mLooperDoctor", LooperDoctor.class);
-        } catch (ReflectiveOperationException e) {
-            throw new ExceptionInInitializerError(e);
-        }
-    }
-
-    /**
-     * Set a Looper Doctor for this Looper.
-     *
-     * This method is NOT thread-safe. It must be called ideally at most once, and never
-     * concurrently on the same looper instance.
-     *
-     * @hide
-     */
-    public void setLooperDoctor(LooperDoctor d) {
-        if (mLooperDoctor == null) {
-            mAlarm = d.notifyLooperStartedLooping(mThread);
-            mLooperDoctor = d;
-            mQueue.setLooperDoctor(d);
-        }
-    }
-
-    /**
-     * Clear the Looper Doctor for this Looper.
-     *
-     * @hide
-     */
-    public void clearLooperDoctor() {
-        LooperDoctor doctor = mLooperDoctor;
-        if (doctor != null && sLooperDoctor.compareAndSet(this, doctor, null)) {
-            doctor.notifyLooperQuit(mAlarm);
-            mQueue.setLooperDoctor(null);
-        }
     }
 
     /**
