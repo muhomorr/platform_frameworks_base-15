arpokrat/arpokrat_frameworks_base

add Private space support in secondary users
Squashed with f2fe6df93672b5d3805de02cc92f1265ea73ac91

services/core/java/com/android/server/pm/UserTypeFactory.java
@@ -307,7 +307,7 @@ public final class UserTypeFactory {
                 .setName(USER_TYPE_PROFILE_PRIVATE)
                 .setBaseType(FLAG_PROFILE)
                 .setProfileParentRequired(true)
-                .setMaxAllowed(1)
+                .setMaxAllowed(getDefaultMaxAllowedSwitchableUsers())
                 .setMaxAllowedPerParent(1)
                 .setEnabled(UserManager.isPrivateProfileEnabled() ? 1 : 0)
                 .setLabels(R.string.profile_label_private)

muhomorr/platform_frameworks_base-15

services/core/java/com/android/server/pm/UserTypeFactory.java
@@ -317,7 +317,7 @@ public final class UserTypeFactory {
                 .setName(USER_TYPE_PROFILE_PRIVATE)
                 .setBaseType(FLAG_PROFILE)
                 .setProfileParentRequired(true)
-                .setMaxAllowed(1)
+                .setMaxAllowed(getDefaultMaxAllowedForSwitchableTypes())
                 .setMaxAllowedPerParent(1)
                 .setEnabled(!android.multiuser.Flags.blockPrivateSpaceCreation()
                         || !ActivityManager.isLowRamDeviceStatic() ? 1 : 0)